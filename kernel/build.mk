f5ba7ae7483567bd0ac57adb0d18d9c7bda2a5ef
Misa <misa@rokid.com>
2018-02-20 03:47:45 -0800

    rewrite Makefiles

--- kernel/build.mk
+++ kernel/build.mk
@@ -3,12 +3,12 @@ KERNEL_DIR=kernel
 
 ARCH_OBJS = $(KERNEL_DIR)/arch/$(arch)/src/irq.o \
 	$(KERNEL_DIR)/arch/$(arch)/src/uart.o \
-	$(KERNEL_DIR)/arch/$(arch)/src/timer.o
+	$(KERNEL_DIR)/arch/$(arch)/src/timer.o \
+	$(KERNEL_DIR)/arch/$(arch)/src/hardware.o
 
 OBJS += $(ARCH_OBJS) \
+	$(KERNEL_DIR)/src/kernel.o \
 	$(KERNEL_DIR)/src/startup.o \
-	$(KERNEL_DIR)/src/hardware.o \
-	$(KERNEL_DIR)/src/$(KERNEL_DIR).o \
 	$(KERNEL_DIR)/src/irq.o \
 	$(KERNEL_DIR)/src/syscalls.o \
 	$(KERNEL_DIR)/src/kalloc.o \
