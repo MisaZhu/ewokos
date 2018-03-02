c34aa227232b8f64ce99d93e374c0a8a0ff3e434
misazhu <misa.zhu@gmail.com>
2018-03-03 02:27:59 +0800

    make sbin/sbin directory

--- sbin/build.mk
+++ sbin/build.mk
@@ -1,11 +1,11 @@
 SBIN_DIR=sbin
 PROGRAM = $(SBIN_DIR)/sbin/shell  \
-
-#	$(SBIN_DIR)/sbin/help
+	$(SBIN_DIR)/sbin/help
 
 EXTRA_CLEAN += $(PROGRAM)
 
 $(SBIN_DIR)/sbin/%: $(SBIN_DIR)/%.c lib/libewok.a
+	mkdir -p sbin/sbin
 	$(CC) $(CFLAGS) -c -o $@.o $<
 	$(LD) -Ttext=100 $@.o lib/libewok.a -o $@
 	rm -f $@.o
