d3ebe9b724aa335c122661d0d0f02dc75ffd4272
May the source be with you <MisaZhu@users.noreply.github.com>
2019-02-04 21:09:36 +0800

    friendly comments

--- kernel/src/mm/kmalloc.h
+++ kernel/src/mm/kmalloc.h
@@ -1,6 +1,8 @@
 #ifndef KMALLOC_H
 #define KMALLOC_H
 
+/*kmalloc/kmfree manage the kernel-reserved memory trunk, the memory trunk is from KMALLOC_BASE, with size KMALLOC_SIZE.*/
+
 #include <types.h>
 
 void* kmalloc(uint32_t size);
