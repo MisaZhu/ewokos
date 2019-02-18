4cb1dbe5943328d80bf1ddc161c51c748601703b
May the source be with you <misa.zhu@gmail.com>
2019-02-19 01:53:59 +0800

    add files

---
+++ system/lib/src/kserv/devserv.c
@@ -0,0 +1,61 @@
+#include "kserv/devserv.h"
+#include "dev.h"
+#include <stdio.h>
+
+void doMount(PackageT* pkg, DeviceT* dev) {
+}
+
+void doUnmount(PackageT* pkg, DeviceT* dev) {
+}
+
+void doOpen(PackageT* pkg, DeviceT* dev) {
+}
+
+void doClose(PackageT* pkg, DeviceT* dev) {
+}
+
+void doWrite(PackageT* pkg, DeviceT* dev) {
+}
+
+void doRead(PackageT* pkg, DeviceT* dev) {
+}
+
+void doInfo(PackageT* pkg, DeviceT* dev) {
+}
+
+void doSetopt(PackageT* pkg, DeviceT* dev) {
+}
+
+void handle(PackageT* pkg, void* p) {
+	DeviceT* dev = (DeviceT*)p;
+  switch(pkg->type) {
+	case DEV_MOUNT:
+		doMount(pkg, dev);
+		break;
+	case DEV_UNMOUNT:
+		doUnmount(pkg, dev);
+		break;
+	case DEV_OPEN:
+		doOpen(pkg, dev);
+		break;
+	case DEV_CLOSE:
+		doClose(pkg, dev);
+		break;
+	case DEV_WRITE:
+		doWrite(pkg, dev);
+		break;
+	case DEV_READ:
+		doRead(pkg, dev);
+		break;
+	case DEV_INFO:
+		doInfo(pkg, dev);
+		break;
+	case DEV_SETOPT:
+		doSetopt(pkg, dev);
+		break;
+	}
+}
+
+bool devServRun(DeviceT *dev) {
+	return kservRun(devGetServName(dev), handle, dev);
+}
---
+++ system/lib/src/kserv/userman.c
@@ -0,0 +1,31 @@
+#include <kserv/userman.h>
+#include <pmessage.h>
+#include <string.h>
+#include <syscall.h>
+#include <stdlib.h>
+
+static int _usermanPid = -1;
+
+#define CHECK_KSERV_USERMAN \
+	if(_usermanPid < 0) \
+		_usermanPid = syscall1(SYSCALL_KSERV_GET, (int)KSERV_USERMAN_NAME); \
+	if(_usermanPid < 0) \
+		return -1; 
+
+
+int usermanAuth(const char* name, const char* passwd) {
+	CHECK_KSERV_USERMAN
+	int uid = -1;
+	char loginData[LOGIN_DATA_LEN*2];
+	strncpy(loginData, name, LOGIN_DATA_LEN);
+	strncpy(loginData+LOGIN_DATA_LEN, passwd, LOGIN_DATA_LEN);
+
+	PackageT* pkg = preq(_usermanPid, USERMAN_AUTH, (void*)loginData, LOGIN_DATA_LEN*2, true);
+	if(pkg == NULL)	
+		return -1;
+
+	uid = *(int*)getPackageData(pkg);
+	free(pkg);
+	return uid;
+}
+
