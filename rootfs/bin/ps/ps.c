ec7887359f2b23ed52f991e225e71dd94adf0a92
misa <misa.zhu@gmail.com>
2019-04-07 01:19:20 +0800

    proc numbers

--- rootfs/bin/ps/ps.c
+++ rootfs/bin/ps/ps.c
@@ -10,12 +10,6 @@ int main() {
 	int num = 0;
 	uint32_t fr_mem = (uint32_t)syscall2(SYSCALL_SYSTEM_CMD, 1, 0) / KB;
 	uint32_t t_mem = (uint32_t)syscall2(SYSCALL_SYSTEM_CMD, 0, 0) / MB;
-	printf("--------------------------------------------------------------\n");
-	if(fr_mem > 1024)
-		printf("memory: total %d MB, free %d KB (%d MB)\n", t_mem, fr_mem, fr_mem/1024);
-	else
-		printf("memory: total %d MB, free %d KB\n", t_mem, fr_mem);
-	printf("--------------------------------------------------------------\n");
 
 	proc_info_t* procs = (proc_info_t*)syscall2(SYSCALL_SYSTEM_CMD, 2, (int)&num);
 	if(procs != NULL) {
@@ -28,8 +22,14 @@ int main() {
 				procs[i].state,
 				procs[i].heap_size);
 		}
-		printf("\n");
 		free(procs);
 	}
+
+	printf("--------------------------------------------------------------\n");
+	if(fr_mem > 1024)
+		printf("memory: total %d MB, free %d KB (%d MB)\n", t_mem, fr_mem, fr_mem/1024);
+	else
+		printf("memory: total %d MB, free %d KB\n", t_mem, fr_mem);
+	printf("processes: %d\n", num);
 	return 0;
 }
