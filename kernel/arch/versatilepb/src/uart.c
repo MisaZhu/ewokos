f3b3a7e841544b66001037d73c5c2e0593492a55
misa <misa.zhu@gmail.com>
2019-04-08 04:22:36 +0800

    uart flags register bug fixed

--- kernel/arch/versatilepb/src/uart.c
+++ kernel/arch/versatilepb/src/uart.c
@@ -6,7 +6,7 @@
 #define UART0 ((volatile uint32_t*)(MMIO_BASE+0x001f1000))
 /* serial port register offsets */
 #define UART_DATA        0x00 
-#define UART_FLAGS       0x06
+#define UART_FLAGS       0x18
 #define UART_INT_ENABLE  0x0e
 #define UART_INT_TARGET  0x0f
 #define UART_INT_CLEAR   0x11
@@ -23,7 +23,6 @@ void uart_dev_init(void) {
 void uart_trans(char c) {
 	/* wait until transmit buffer is full */
 	while (UART0[UART_FLAGS] & UART_TRANSMIT);
-
 	/* write the character */
 	UART0[UART_DATA] = c;
 }
