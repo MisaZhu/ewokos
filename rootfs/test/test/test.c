37b427464aa362a3983133ddfbf87ae712675c66
misazhu <misa.zhu@gmail.com>
2019-04-13 13:12:50 +0000

    sd ext2 test

--- rootfs/test/test/test.c
+++ rootfs/test/test/test.c
@@ -1,10 +1,33 @@
 #include <types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <syscall.h>
+#include <ext2.h>
+
+int32_t read_block(int32_t block, char* buf) {
+	if(syscall2(SYSCALL_SDC_READ, block, (int32_t)buf) < 0)
+		return -1;
+
+	int32_t res = -1;
+	while(true) {
+		res = syscall0(SYSCALL_SDC_READ_DONE);
+		if(res != 0)
+			break;
+		yield();
+	}
+
+	if(res < 0)
+		return -1;
+	return 0;
+}
 
 int main() {
-	int i = 0;
-	printf("Hello, world! %d\n", i);
+	int32_t size;
+	char* p = ext2_load("/sbin/init", read_block, malloc, &size);
+		
+	if(p != NULL)
+		free(p);
 	return 0;
 }
 
