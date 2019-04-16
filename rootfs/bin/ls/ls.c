fb43ddbb8dd27467e7252f543612022befbf9fbc
misa <misa.zhu@gmail.com>
2019-04-17 03:09:25 +0800

    ls with dir_name

--- rootfs/bin/ls/ls.c
+++ rootfs/bin/ls/ls.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <vfs/fs.h>
 #include <stdlib.h>
+#include <kstring.h>
 
 #define ALIGN_LEN 16
 static const char* align_cmd(const char* cmd) {
@@ -22,10 +23,28 @@ static const char* align_cmd(const char* cmd) {
 	return ret;
 }
 
-
 int main() {
+	char name[NAME_MAX];
 	char pwd[NAME_MAX];
-	int fd = fs_open(getcwd(pwd, NAME_MAX), 0);
+	getcwd(pwd, NAME_MAX);
+
+	init_cmain_arg();
+	const char* arg = read_cmain_arg();
+	arg = read_cmain_arg();
+	if(arg == NULL) {
+		strcpy(name, pwd);
+	}
+	else if(arg[0] == '/') {
+		strcpy(name, arg);
+	}
+	else {
+		if(strcmp(pwd, "/") == 0)
+			snprintf(name, NAME_MAX-1, "/%s", arg);
+		else
+			snprintf(name, NAME_MAX-1, "%s/%s", pwd, arg);
+	}
+
+	int fd = fs_open(name, 0);
 	if(fd >= 0) {
 		uint32_t num;
 		fs_info_t* infos = fs_kids(fd, &num);
