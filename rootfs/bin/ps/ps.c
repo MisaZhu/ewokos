6d4b6c4402681f289a342ec34fc8e76c889392fe
misazhu <misa.zhu@gmail.com>
2019-04-17 22:52:20 +0800

    ps format output

--- rootfs/bin/ps/ps.c
+++ rootfs/bin/ps/ps.c
@@ -6,40 +6,23 @@
 #include <kstring.h>
 #include <cmain.h>
 
-#define ALIGN_LEN 16
-static const char* align_cmd(const char* cmd) {
-	static char ret[ALIGN_LEN+1];
-	int32_t i = 0;
-
-	while(i< ALIGN_LEN) {
-		ret[i] = cmd[i];
-		if(cmd[i] == 0)
-			break;
-		i++;
-	}
-	
-	while(i< ALIGN_LEN) {
-		ret[i++] = ' ';
-	}
-	ret[i] = 0;
-	return ret;
-}
-
 int main() {
 	int num = 0;
 	uint32_t fr_mem = (uint32_t)syscall2(SYSCALL_SYSTEM_CMD, 1, 0) / KB;
 	uint32_t t_mem = (uint32_t)syscall2(SYSCALL_SYSTEM_CMD, 0, 0) / MB;
+	uint32_t sec;
+	syscall3(SYSCALL_SYSTEM_CMD, 4, (int32_t)&sec, 0);
 
 	proc_info_t* procs = (proc_info_t*)syscall2(SYSCALL_SYSTEM_CMD, 2, (int)&num);
 	if(procs != NULL) {
 		for(int i=0; i<num; i++) {
-			const char* cmd = align_cmd(procs[i].cmd);
-			printf("%s pid:%d, father:%d, owner:%d, state: %d heap_size: %d\n", 
-				cmd,
+			printf("%16s pid:%4d father:%4d owner:%4d state: %4d cpu_time: %8d heap_size: %d\n", 
+				procs[i].cmd,
 				procs[i].pid,
 				procs[i].father_pid,
 				procs[i].owner,
 				procs[i].state,
+				sec - procs[i].start_sec,
 				procs[i].heap_size);
 		}
 		free(procs);
