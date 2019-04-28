ac03ca4e0419fcbd18975e80c30071fbaea2c130
misa <misa.zhu@gmail.com>
2019-04-29 06:25:13 +0800

    simple config comment

--- rootfs/lib/src/sconf.c
+++ rootfs/lib/src/sconf.c
@@ -3,6 +3,20 @@
 #include <kstring.h>
 #include <stdlib.h>
 
+static inline bool is_space(char c) {
+	return (c == ' ' || c == '\t' || c == '\r');
+}
+
+static inline void trim_right(char* s, int32_t i) {
+	i--;
+	while(i >= 0) {
+		if(is_space(s[i]))
+			s[i--] = 0;
+		else
+			break;
+	}
+}
+
 sconf_t* sconf_load(const char* fname) {
 	if(fname == NULL || fname[0] == 0)
 		return NULL;
@@ -19,7 +33,7 @@ sconf_t* sconf_load(const char* fname) {
 	
 	int32_t i = 0;	
 	int32_t it = 0;	/*item index*/
-	uint8_t stat = 0; /*0 for name; 1 for value*/
+	uint8_t stat = 0; /*0 for name; 1 for value; 2 for comment*/
 	sconf_item_t* item = &conf->items[0];
 	while(it < S_CONF_ITEM_MAX) {
 		char c;
@@ -28,27 +42,54 @@ sconf_t* sconf_load(const char* fname) {
 			break;
 		}
 
-		if(stat == 0) {/*read name*/
+		if(i == 0 && is_space(c)) {
+			continue;
+		}
+		else if(c == '#') { //comment
+			if(stat == 1) {
+				item->value[i] = 0;
+				trim_right(item->value, i);
+				it++;
+				item = &conf->items[it];
+			}
+			stat = 2;
+			continue;
+		}
+		else if(stat == 0) {/*read name*/
 			if(c == '=') {
+				item->name[i] = 0;
 				i = 0;
 				stat = 1;
 				continue;
 			}
-			if(i < S_CONF_NAME_MAX)
+			else if(is_space(c)) {
+				continue;
+			}
+			else if(i < S_CONF_NAME_MAX) {
 				item->name[i] = c;
-			i++;
+				i++;
+			}
 		}	
-		else { /*read value*/
+		else if(stat == 1) { /*read value*/
 			if(c == '\n') {
+				item->value[i] = 0;
+				trim_right(item->value, i);
 				i = 0;
 				stat = 0;
 				it++;
 				item = &conf->items[it];
 				continue;
 			}
-			if(i < S_CONF_VALUE_MAX)
+			if(i < S_CONF_VALUE_MAX) {
 				item->value[i] = c;
-			i++;
+				i++;
+			}
+		}
+		else { //comment
+			if(c == '\n') {
+				i = 0;
+				stat = 0;
+			}
 		}
 	}
 	if(it < S_CONF_ITEM_MAX) 
