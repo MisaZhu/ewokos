e2862ef3a80e8ad12ec86aa9dbdcbf90be45d996
misazhu <misa.zhu@gmail.com>
2019-05-02 17:35:11 +0800

    mouse test bug fixed

--- rootfs/test/mouse/mouse.c
+++ rootfs/test/mouse/mouse.c
@@ -12,8 +12,11 @@ int main() {
 	while(true) {
 		int8_t ev[4];
 		int sz = read(fd, ev, 4);
-		if(sz <= 0)
+		if(sz < 0)
 			break;
+		if(sz == 0)
+			continue;
+	
 		printf("mouse: down:0x%x, rx:%d, ry:%d, rz:%d\n", ev[0], ev[1], ev[2], ev[3]);
 		if(ev[0] == 1)
 			break;
