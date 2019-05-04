3594e89547d5f92a3640330fbb9feec37c83e9fa
misa <misa.zhu@gmail.com>
2019-05-05 03:31:10 +0800

    cat write non-block

--- rootfs/bin/cat/cat.c
+++ rootfs/bin/cat/cat.c
@@ -6,23 +6,29 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-	char fname_r[FULL_NAME_MAX];
+	char fname[FULL_NAME_MAX];
 	if(argc < 2)
 		return -1;
-	fs_full_name(argv[1], fname_r, FULL_NAME_MAX);
-	int fd_r = open(fname_r, O_RDONLY);
-	if(fd_r < 0)
+	fs_full_name(argv[1], fname, FULL_NAME_MAX);
+	int fd = open(fname, O_RDONLY);
+	if(fd < 0) {
+		printf("'%s' open failed!\n", fname);
 		return -1;
+	}
 
 	int fd_w = _stdout;
 	while(true) {
-		char buf[128+1];
-		int sz = read(fd_r, buf, 128);
+		char buf[128];
+		int sz = read(fd, buf, 128);
 		if(sz <= 0)
 			break;
-		buf[sz] = 0;
-		write(fd_w, buf, sz);
+		while(true) { //non-block
+			int res = write(fd_w, buf, sz);
+			if(res < 0 && errno == EAGAIN)
+				continue;
+			break;
+		}
 	}
-	close(fd_r);
+	close(fd);
 	return 0;
 }
