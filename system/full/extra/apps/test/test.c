39d466ab32adc55dcebadc2c79ac6567ced9e243
misa <misa.zhu@gmail.com>
2020-05-18 04:53:50 +0800

    tinysys with mem fs

--- system/full/extra/apps/test/test.c
+++ system/full/extra/apps/test/test.c
@@ -11,13 +11,15 @@ int main(int argc, char** argv) {
 	int i=0;
 	proto_t in;
 	PF->init(&in, NULL, 0)->adds(&in, "hello!");
+	proto_t r;
+	PF->init(&r, NULL, 0);
 	while(1) {
 		set_global("test.test", &in);	
 		usleep(1000);
-		proto_t* r = get_global("test.test");	
-		if(r != NULL) {
-			printf("ret:'%s'\n", proto_read_str(r));
+		if(get_global("test.test", &r) == 0) {	
+			printf("ret:'%s'\n", proto_read_str(&r));
 		}
+		PF->clear(&r);
 		i++;
 	}
 	PF->clear(&in);
