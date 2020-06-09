c0463f4b6bc40b0a935da8e4ea5ac4dc805005b8
misa.zhu <misa.zhu@gmail.com>
2020-06-09 12:56:41 +0800

    alpha window

--- system/extra/apps/png/png.c
+++ system/extra/apps/png/png.c
@@ -7,14 +7,7 @@
 #include <upng/upng.h>
 
 static void draw(graph_t* g, graph_t* img) {
-	int x, y;
-	clear(g, 0xff444444);
-	for(y=10; y<(int32_t)g->h; y+=16) {
-		for(x=0; x<(int32_t)g->w; x+=16) {
-			fill(g, x, y, 6, 6, 0xff888888);
-		}
-	}
-
+	clear(g, 0x66ffffff);
 	blt_alpha(img, 0, 0, img->w, img->h,
 			g, 0, 0, img->w, img->h, 0xff);
 }
@@ -39,7 +32,7 @@ int main(int argc, char* argv[]) {
 	}
 	printf("ok\n");
 
-	x_t* x = x_open(10, 10, img->w, img->h+20, "png", X_STYLE_NORMAL | X_STYLE_NO_RESIZE);
+	x_t* x = x_open(10, 10, img->w, img->h+20, "png", X_STYLE_NORMAL | X_STYLE_NO_RESIZE | X_STYLE_ALPHA);
 	if(x == NULL) {
 		graph_free(img);
 		return -1;
