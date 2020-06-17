8a05ffc2cd3f236ba96e4e6183beb7787e6c6975
misa.zhu <misa.zhu@gmail.com>
2020-06-17 18:51:46 +0800

    draw line bug fixed

--- system/libs/graph/src/graph.c
+++ system/libs/graph/src/graph.c
@@ -116,14 +116,36 @@ void graph_reverse(graph_t* g) {
 void graph_line(graph_t* g, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint32_t color) {
 	if(g == NULL)
 		return;
+
+	if(x1 == x2 && y1 == y2) {
+		if(!has_alpha(color))
+			graph_pixel_safe(g, x1, y1, color);
+		else
+			pixel_argb_safe(g, x1, y1,
+					(color >> 24) & 0xff,
+					(color >> 16) & 0xff,
+					(color >> 8) & 0xff,
+					color & 0xff);
+		return;
+	}
+
 	int32_t dx, dy, x, y, s1, s2, e, temp, swap, i;
 
 	dy = y2 - y1;
-	if (dy<0) { dy = -dy; s2 = -1; }
-	else s2 = 1;
+	if (dy<0) {
+		dy = -dy; 
+		s2 = -1; 
+	}
+	else
+		s2 = 1;
+
 	dx = x2 - x1;
-	if (dx<0) { dx = -dx; s1 = -1; }
-	else s1 = 1;
+	if (dx<0) {
+		dx = -dx; 
+		s1 = -1;
+	}
+	else
+		s1 = 1;
 
 	x = x1;
 	y = y1;
@@ -133,36 +155,46 @@ void graph_line(graph_t* g, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint
 		dy=temp;
 		swap=1;
 	}
-	else swap=0;
+	else
+		swap=0;
 
 	e = 2 * dy - dx;
 	if(!has_alpha(color)) {
 		for (i=0; i<=dx; i++) {
 			graph_pixel_safe(g, x, y, color);
 			while (e>=0) {
-				if (swap==1) x += s1;
-				else y += s2;
+				if (swap==1)
+					x += s1;
+				else
+					y += s2;
 				e -= 2*dx;
 			}
-			if (swap==1) y += s2;
-			else x += s1;
+			if (swap==1)
+				y += s2;
+			else
+				x += s1;
 			e += 2*dy;
 		}
 	}
 	else {
+		uint8_t ca = (color >> 24) & 0xff;
+		uint8_t cr = (color >> 16) & 0xff;
+		uint8_t cg = (color >> 8) & 0xff;
+		uint8_t cb = (color) & 0xff;
+	
 		for (i=0; i<=dx; i++) {
-			pixel_argb_safe(g, x, y,
-					(color >> 24) & 0xff,
-					(color >> 16) & 0xff,
-					(color >> 8) & 0xff,
-					color & 0xff);
+			pixel_argb_safe(g, x, y, ca, cr, cg, cb);
 			while (e>=0) {
-				if (swap==1) x += s1;
-				else y += s2;
+				if (swap==1)
+					x += s1;
+				else 
+					y += s2;
 				e -= 2*dx;
 			}
-			if (swap==1) y += s2;
-			else x += s1;
+			if (swap==1)
+				y += s2;
+			else
+				x += s1;
 			e += 2*dy;
 		}
 	}
