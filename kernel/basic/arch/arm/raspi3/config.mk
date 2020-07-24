70aa72687703312ef126266f8140367d964696b6
misa.zhu <misa.zhu@gmail.com>
2020-07-24 15:58:43 +0800

    remove raspi3

--- kernel/basic/arch/arm/raspi3/config.mk
+++
@@ -1,4 +0,0 @@
-CPU = arm1176jzf-s
-#QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 512M -serial mon:stdio -nographic -display none
-QEMU_FLAGS = -cpu arm1176 -M raspi3 -m 512M -serial mon:stdio
-ARCH_QEMU_CFLAGS = -mcpu=$(CPU) -DA_CORE
--- kernel/basic/arch/arm/raspi3/timer_arch.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_ARCH_H
-#define TIMER_ARCH_H
-
-#include <stdint.h>
-
-extern uint32_t _timer_frq;
-void write_cntv_tval(uint32_t val);
-
-#endif
