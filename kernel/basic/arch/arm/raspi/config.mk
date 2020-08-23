84f2144012a57e2679913e0adfce3298a8a5cbb2
misazhu <misa.zhu@gmail.com>
2020-08-23 16:05:06 +0800

    config.mk

--- kernel/basic/arch/arm/raspi/config.mk
+++
@@ -1,4 +0,0 @@
-CPU = arm1176jzf-s
-#QEMU_FLAGS = -cpu arm1176 -M raspi -m 512M -serial mon:stdio -nographic -display none
-QEMU_FLAGS = -cpu arm1176 -M raspi -m 512M -serial mon:stdio
-ARCH_QEMU_CFLAGS = -mcpu=$(CPU) -DRASPI
--- kernel/basic/arch/arm/versatilepb/config.mk
+++
@@ -1,4 +0,0 @@
-CPU = arm926ej-s
-#QEMU_FLAGS = -cpu arm926 -M versatilepb -m 256M -nographic -display none -serial mon:stdio
-QEMU_FLAGS = -cpu arm926 -M versatilepb -m 256M -serial mon:stdio
-ARCH_QEMU_CFLAGS = -mcpu=$(CPU) -DVERSATILEPB
