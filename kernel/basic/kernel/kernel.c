6c9687a81f628deb28dd6675159224e5ac2a8d65
Misa.Z <misa@MisaZs-MacBook-Pro.local>
2020-08-31 16:42:21 +0800

    update

--- kernel/basic/kernel/kernel.c
+++ kernel/basic/kernel/kernel.c
@@ -20,9 +20,12 @@
 #include <dev/sd.h>
 #endif
 
+#ifdef DISPLAY
+#include <kconsole.h>
+#endif
+
 #ifdef FRAMEBUFFER
 #include <dev/framebuffer.h>
-#include <kconsole.h>
 #endif
 
 page_dir_entry_t* _kernel_vm = NULL;
@@ -42,6 +45,7 @@ static void __attribute__((optimize("O0"))) copy_interrupt_table(void) {
 	}
 }
 
+//static void __attribute__((optimize("O0"))) set_kernel_init_vm(page_dir_entry_t* vm) {
 static void set_kernel_init_vm(page_dir_entry_t* vm) {
 	memset(vm, 0, PAGE_DIR_SIZE);
 
@@ -58,6 +62,7 @@ static void set_kernel_init_vm(page_dir_entry_t* vm) {
 	arch_vm(vm);
 }
 
+//void __attribute__((optimize("O0"))) set_kernel_vm(page_dir_entry_t* vm) {
 void set_kernel_vm(page_dir_entry_t* vm) {
 	set_kernel_init_vm(vm);
 	map_pages(vm, 
@@ -67,6 +72,7 @@ void set_kernel_vm(page_dir_entry_t* vm) {
 		AP_RW_D, 0);
 }
 
+//static void __attribute__((optimize("O0"))) init_kernel_vm(void) {
 static void init_kernel_vm(void) {
 	int32_t i;
 	for(i=0; i<RAM_HOLE_MAX; i++)
--- kernel/extra/lib/src/kconsole.c
+++ kernel/extra/lib/src/kconsole.c
@@ -2,12 +2,14 @@
 #include <graph.h>
 #include <console.h>
 #include <stddef.h>
+#include <kprintf.h>
 
 static console_t _console;
 
 #ifdef WITH_LCDHAT
 #include "lcdhat/lcdhat.h"
 void kconsole_init(void) {
+	printf("kernel: console with lcdhat out\n");
 	console_init(&_console);
 	lcd_init();
 	graph_t* g = graph_new(NULL, LCD_WIDTH, LCD_HEIGHT);
@@ -26,19 +28,19 @@ void kconsole_setup(void) {
 #else
 
 void kconsole_init(void) {
+	printf("kernel: console with framebuffer out\n");
 	console_init(&_console);
 }
 
 void kconsole_setup(void) {
-	if(_console.g != NULL) //already setup
-		return;
-
+	_console.g = NULL;
 	fbinfo_t* info = fb_get_info();
 	if(info->pointer == 0)
 		return;
 
 	graph_t* g = graph_new(NULL, info->width, info->height);
 	_console.g = g;
+	_console.font = font_by_name("8x16");
 	console_reset(&_console);
 }
 
