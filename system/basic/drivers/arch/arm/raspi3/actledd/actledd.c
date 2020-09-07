0ebf419c3ae048ac821ea7606d7431122f5da104
misazhu <misa.zhu@gmail.com>
2020-09-08 03:48:57 +0800

    pi3 actled

--- system/basic/drivers/arch/arm/raspi3/actledd/actledd.c
+++ system/basic/drivers/arch/arm/raspi3/actledd/actledd.c
@@ -1,18 +1,29 @@
-#include <arch/bcm283x/gpio.h>
+#include <arch/bcm283x/mailbox.h>
 #include <sys/vdevice.h>
 #include <string.h>
 
 static void actled(bool on) {
-  uint32_t ra;
-  ra = get32(GPIO_FSEL4);
-  ra &= ~(7<<21);
-  ra |= 1<<21;
-  put32(GPIO_FSEL4, ra);
+	mail_message_t msg;
+	/*message head + tag head + property*/
+	uint32_t size = 12 + 12 + 8;
+	uint32_t buf[8] __attribute__((aligned(16)));
 
-  if(!on)
-    put32(GPIO_CLR1, 1<<(47-32));
-  else
-    put32(GPIO_SET1, 1<<(47-32));
+	/*message head*/
+	buf[0] = size;
+	buf[1] = 0;	//RPI_FIRMWARE_STATUS_REQUEST;
+	/*tag head*/
+	buf[2] = 0x00038041;								/*tag*/
+	buf[3] = 8;									/*buffer size*/
+	buf[4] = 0;									/*respons size*/
+	/*property package*/
+	buf[5] =  130;				/*actled pin number*/
+	buf[6] =  on ? 1: 0;								/*property value*/
+	/*message end*/
+	buf[7] = 0;
+	
+	msg.data = ((uint32_t)buf + 0x40000000) >> 4;	
+	mailbox_send(PROPERTY_CHANNEL, &msg);
+	mailbox_read(PROPERTY_CHANNEL, &msg);
 }
 
 static int actled_write(int fd, int from_pid, fsinfo_t* info,
@@ -49,7 +60,7 @@ static int actled_dev_cntl(int from_pid, int cmd, proto_t* in, proto_t* ret, voi
 
 int main(int argc, char** argv) {
 	const char* mnt_point = argc > 1 ? argv[1]: "/dev/actled";
-	gpio_init();
+	_mmio_base = mmio_map();
 
 	vdevice_t dev;
 	memset(&dev, 0, sizeof(vdevice_t));
