eb9327db5d7ee0073f7e7f10c936993c83d4f75f
misazhu <misa.zhu@gmail.com>
2020-09-14 01:19:03 +0800

    raspi2/3 arch

--- kernel/basic/arch/arm/raspi2/uart.c
+++ kernel/basic/arch/arm/raspi2/uart.c
@@ -1,6 +1,5 @@
 #include <dev/uart.h>
 
-#ifdef RASPI2
 #include "bcm283x/pl011_uart.h"
 int32_t uart_dev_init(void) {
 	return pl011_uart_init();
@@ -9,13 +8,4 @@ int32_t uart_dev_init(void) {
 int32_t uart_write(const void* data, uint32_t size) {
 	return pl011_uart_write(data, size);
 }
-#else
-#include "bcm283x/mini_uart.h"
-int32_t uart_dev_init(void) {
-	return mini_uart_init();
-}
 
-int32_t uart_write(const void* data, uint32_t size) {
-	return mini_uart_write(data, size);
-}
-#endif
---
+++ kernel/basic/arch/arm/raspi3/hw_info.c
@@ -0,0 +1,29 @@
+#include <kernel/hw_info.h>
+#include <kernel/system.h>
+#include <kstring.h>
+#include "bcm283x/mailbox.h"
+#include "bcm283x/cpu_freq.h"
+
+static hw_info_t _hw_info;
+
+void hw_info_init(void) {
+	strcpy(_hw_info.machine, "raspi3");
+	_hw_info.phy_mem_size = 1024*MB;
+	_hw_info.phy_mmio_base = 0x3F000000;
+	_hw_info.mmio_size = 16*MB;
+}
+
+inline hw_info_t* get_hw_info(void) {
+	return &_hw_info;
+}
+
+void arch_vm(page_dir_entry_t* vm) {
+	uint32_t offset = 0x40000000 - _hw_info.phy_mmio_base; //CORE0_ROUTING
+	uint32_t vbase = MMIO_BASE + offset;
+	uint32_t pbase = _hw_info.phy_mmio_base + offset;
+	map_pages(vm, vbase, pbase, pbase+16*KB, AP_RW_D, 1);
+}
+
+void hw_optimise(void) {
+	cpu_freq_init();	
+}
