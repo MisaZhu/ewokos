db9967a4e2a60605f287941304417daaa1769229
misazhu <misa.zhu@gmail.com>
2020-09-19 02:29:51 +0800

    sysinfo

--- system/basic/bin/uname/uname.c
+++ system/basic/bin/uname/uname.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/syscall.h>
+#include <sysinfo.h>
 
 int main(int argc, char* argv[]) {
+	sysinfo_t sysinfo;
+	syscall1(SYS_GET_SYSINFO, (int32_t)&sysinfo);
+
 	if(argc == 1)
 		printf("Ewok\n");
 	else if(strcmp(argv[1], "-a") == 0)
-		printf("Ewok micro-kernel OS, ARM A-Core, V 0.01\n");
+		printf("Ewok micro-kernel OS, %s, V 0.01\n", sysinfo.machine);
 	return 0;
 }
