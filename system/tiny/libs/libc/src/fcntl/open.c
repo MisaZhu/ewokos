c10801df590546242a2b2512adc709b298b6601d
misazhu <misa.zhu@gmail.com>
2020-09-21 03:54:42 +0800

    create bug fixed

--- system/tiny/libs/libc/src/fcntl/open.c
+++ system/tiny/libs/libc/src/fcntl/open.c
@@ -7,12 +7,14 @@
 
 int open(const char* fname, int oflag) {
 	int fd = -1;
+	bool created = false;
 	fsinfo_t info;
 
 	if(vfs_get(fname, &info) != 0) {
 		if((oflag & O_CREAT) != 0) {
 			if(vfs_create(fname, &info, FS_TYPE_FILE) != 0)
 				return -1;
+			created = true;
 		}
 		else  {
 			return -1;	
@@ -20,8 +22,11 @@ int open(const char* fname, int oflag) {
 	}
 
 	fd = vfs_open(&info, oflag);
-	if(fd < 0)
+	if(fd < 0) {
+		if(created)
+			vfs_del(&info);
 		return -1;
+	}
 	
 	proto_t in, out;
 	PF->init(&out, NULL, 0);
@@ -34,6 +39,8 @@ int open(const char* fname, int oflag) {
 	if(ipc_call(info.mount_pid, FS_CMD_OPEN, &in, &out) != 0 ||
 			proto_read_int(&out) != 0) {
 		vfs_close(fd);
+		if(created)
+			vfs_del(&info);
 		fd = -1;
 	}
 
