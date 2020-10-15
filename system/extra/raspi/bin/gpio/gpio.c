ec29f604527247dc3863aec49eacb257bf11d381
misazhu <misa.zhu@gmail.com>
2020-10-16 02:42:55 +0800

    nxez keys leds

--- system/extra/raspi/bin/gpio/gpio.c
+++ system/extra/raspi/bin/gpio/gpio.c
@@ -9,16 +9,24 @@ int main(int argc, char* argv[]) {
 	int pin = 0;
 	int v = 0;
 	if(argc < 2) {
-		printf("Usage: gpio <pin_no> {1/0}\n");
+		printf("Usage: gpio <pin_no> {1/0/u/d}\n");
 		return -1;
 	}
 
 	pin = atoi(argv[1]);
 	if(argc > 2) {
-		v = atoi(argv[2]);
-		bcm283x_gpio_pull(pin, GPIO_PULL_DOWN);
-		bcm283x_gpio_config(pin, GPIO_OUTPUT);
-		bcm283x_gpio_write(pin, v);
+		if(argv[2][0] == 'u') {
+			bcm283x_gpio_pull(pin, GPIO_PULL_UP);
+		}
+		else if(argv[2][0] == 'd') {
+			bcm283x_gpio_pull(pin, GPIO_PULL_DOWN);
+		}
+		else {
+			v = atoi(argv[2]);
+			bcm283x_gpio_pull(pin, GPIO_PULL_DOWN);
+			bcm283x_gpio_config(pin, GPIO_OUTPUT);
+			bcm283x_gpio_write(pin, v);
+		}
 	}
 	else {
 		bcm283x_gpio_config(pin, GPIO_INPUT);
