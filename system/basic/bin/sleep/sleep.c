40dde98feca4a023d6581ea843dd514c4273c21e
misa.zhu <misa.zhu@gmail.com>
2020-10-20 17:43:40 +0800

    cmd sleep

---
+++ system/basic/bin/sleep/sleep.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+	if(argc < 2)
+		return -1;
+
+	sleep(atoi(argv[1]));
+	return 0;
+}
+
