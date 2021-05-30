59615827f42196119654805e9c134658c8486aa5
misazhu <misa.zhu@gmail.com>
2021-05-31 02:52:25 +0800

    multi core start flag

--- kernel/basic/kernel/src/core.c
+++ kernel/basic/kernel/src/core.c
@@ -9,12 +9,12 @@ static uint32_t _multi_core_flag = 0x0;
 inline void start_multi_cores(uint32_t cores) {
 	(void)cores;
 
-	_multi_core_flag = 0x12345678;
 	uint32_t i;
 	for(i=0; i<CPU_MAX_CORES; i++) {
 		_cpu_cores[i].actived = 0;
 	}
 	_cpu_cores[0].actived = 1;
+	_multi_core_flag = 0x12345678;
 }
 
 inline int32_t multi_cores_ready(void) {
