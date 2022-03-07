0449fcf75ac00a6c881cda93ce6f762a587332d7
May the source be with you <misa.zhu@gmail.com>
2022-03-07 16:17:55 +0800

    xim none(basic keyboard)

--- kernel/build/raspi2/config.mk
+++ kernel/build/raspi2/config.mk
@@ -5,5 +5,5 @@ QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 1024M -serial mon:stdio
 ARCH_CFLAGS = -march=armv7ve
 ARCH=arm/v7
 BSP=raspi2
-SYS_LOAD_MODE=sd
-#SYS_LOAD_MODE=romfs
+#SYS_LOAD_MODE=sd
+SYS_LOAD_MODE=romfs
--- system/full/etc/init.rd
+++ system/full/etc/init.rd
@@ -4,7 +4,7 @@
 /sbin/x/xmoused /dev/mouse0
 /sbin/x/xjoystickd /dev/joystick revx
 
-#/sbin/x/xim_none
-/sbin/x/xim_vkey
+/sbin/x/xim_none
+#/sbin/x/xim_vkey
 
 /bin/x/launcher
