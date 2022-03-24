49041b2e9fbac995e68d3ea2eb4875af04bb243f
May the source be with you <misa.zhu@gmail.com>
2022-03-24 16:07:41 +0800

    Update config.mk

--- kernel/build/raspi3a/config.mk
+++ kernel/build/raspi3a/config.mk
@@ -1,7 +1,7 @@
 CPU = cortex-a53
 QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 512M -serial mon:stdio
-#ARCH_CFLAGS = -mcpu=cortex-a53 -DPI3A -DKERNEL_SMP
-ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI3A
+ARCH_CFLAGS = -mcpu=cortex-a53 -DPI3A -DKERNEL_SMP
+#ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI3A
 ARCH=arm/v7
 BSP=raspi3
 SYS_LOAD_MODE=sd
