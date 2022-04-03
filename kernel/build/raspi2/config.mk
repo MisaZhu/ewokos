6b6e5cb430a2f20f344ad22660a02eec405df4bf
misa.zhu <misa.zhu@gmail.com>
2022-04-03 15:20:42 +0800

    Update config.mk

--- kernel/build/raspi2/config.mk
+++ kernel/build/raspi2/config.mk
@@ -1,6 +1,6 @@
 CPU = cortex-a7
 #QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 1024M -serial mon:stdio -nographic -display none
-QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 1024M -serial mon:stdio
+QEMU_FLAGS = -cpu $(CPU) -M raspi2b -m 1024M -serial mon:stdio
 ARCH_CFLAGS = -march=armv7ve -DKERNEL_SMP
 #ARCH_CFLAGS = -march=armv7ve
 ARCH=arm/v7
