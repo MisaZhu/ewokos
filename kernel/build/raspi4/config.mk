c2ce8437782e1703e22ad4a93325cbca9ab009e0
May the source be with you <misa.zhu@gmail.com>
2022-04-06 00:40:12 +0800

    try qemu aarch64

--- kernel/build/raspi4/config.mk
+++ kernel/build/raspi4/config.mk
@@ -1,8 +1,8 @@
-CPU = cortex-a7
-QEMU_FLAGS = -cpu $(CPU) -M raspi2 -m 1024M -serial mon:stdio
-#ARCH_CFLAGS = -march=armv7ve -DKERNEL_SMP
-ARCH_CFLAGS = -march=armv7ve
+CPU = cortex-a53
+QEMU_FLAGS = -cpu $(CPU) -M raspi3 -m 1024M -serial mon:stdio
+ARCH_CFLAGS = -mcpu=cortex-a53 -DKERNEL_SMP
+#ARCH_CFLAGS =  -mcpu=cortex-a53
 ARCH=arm/v7
-BSP=raspi4
+BSP=raspi3
 SYS_LOAD_MODE=sd
 #SYS_LOAD_MODE=romfs
