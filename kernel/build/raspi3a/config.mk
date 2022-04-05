d5afa3192ca1118bfa81c9ed6e28baf3051c1c91
May the source be with you <misa.zhu@gmail.com>
2022-04-06 01:11:52 +0800

    raspix bsp merge

--- kernel/build/raspi3a/config.mk
+++ kernel/build/raspi3a/config.mk
@@ -3,6 +3,6 @@ QEMU_FLAGS = -cpu $(CPU) -M raspi3 -m 512M -serial mon:stdio
 ARCH_CFLAGS = -mcpu=cortex-a53 -DPI3A -DKERNEL_SMP
 #ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI3A
 ARCH=arm/v7
-BSP=raspi3
+BSP=raspix
 SYS_LOAD_MODE=sd
 #SYS_LOAD_MODE=romfs
--- kernel/build/raspi4/config.mk
+++ kernel/build/raspi4/config.mk
@@ -1,8 +1,8 @@
 CPU = cortex-a53
 QEMU_FLAGS = -cpu $(CPU) -M raspi3 -m 1024M -serial mon:stdio
-ARCH_CFLAGS = -mcpu=cortex-a53 -DKERNEL_SMP
-#ARCH_CFLAGS =  -mcpu=cortex-a53
+ARCH_CFLAGS = -mcpu=cortex-a53 -DKERNEL_SMP -DPI4
+#ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI4
 ARCH=arm/v7
-BSP=raspi3
+BSP=raspix
 SYS_LOAD_MODE=sd
 #SYS_LOAD_MODE=romfs
