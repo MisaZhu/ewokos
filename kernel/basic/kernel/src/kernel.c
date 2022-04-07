b3a59d2d475deb5ecfcf32e43216a146ca2f6acc
misazhu <misa.zhu@gmail.com>
2022-04-08 01:14:58 +0800

    set kernel vm lock safe

--- kernel/basic/kernel/src/kernel.c
+++ kernel/basic/kernel/src/kernel.c
@@ -102,8 +102,8 @@ void __attribute__((optimize("O0"))) _slave_kernel_entry_c(void) {
 		if(multi_cores_ready() == 0)
 			break;
 	}
-	set_translation_table_base(V2P((uint32_t)_kernel_vm));
 	kernel_lock();
+	set_translation_table_base(V2P((uint32_t)_kernel_vm));
 	//printf("kernel: start core %d\n", get_core_id());
 	_started_cores++;
 	kernel_unlock();
