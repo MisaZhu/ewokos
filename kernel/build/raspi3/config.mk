21cde67ff1e552cfb69aa235aef14f4b5e23ca00
misazhu <misa.zhu@gmail.com>
2022-04-21 01:05:31 +0800

    sdload only

--- kernel/build/raspi3/config.mk
+++ kernel/build/raspi3/config.mk
@@ -1,6 +1,5 @@
 CPU = cortex-a53
-QEMU_FLAGS = -cpu $(CPU) -M raspi3 -m 1024M -serial mon:stdio
+QEMU_FLAGS = -cpu $(CPU) -M raspi3b -m 1024M -serial mon:stdio
 ARCH_CFLAGS = -mcpu=cortex-a53 -DKERNEL_SMP -DPI3
-#ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI3
 ARCH=arm/v7
 BSP=raspix
--- kernel/build/raspi3a/config.mk
+++ kernel/build/raspi3a/config.mk
@@ -1,6 +1,5 @@
 CPU = cortex-a53
-QEMU_FLAGS = -cpu $(CPU) -M raspi3 -m 512M -serial mon:stdio
+QEMU_FLAGS = -cpu $(CPU) -M raspi3ap -m 512M -serial mon:stdio
 ARCH_CFLAGS = -mcpu=cortex-a53 -DPI3A -DKERNEL_SMP
-#ARCH_CFLAGS =  -mcpu=cortex-a53 -DPI3A
 ARCH=arm/v7
 BSP=raspix
