6c3e8c7b309044f8d8f25b586190f99c57fae3c3
May the source be with you <misa.zhu@gmail.com>
2022-04-30 17:51:01 +0800

    update

--- system/full/x/bin/gtest/gtest.cc
+++ system/full/x/bin/gtest/gtest.cc
@@ -149,8 +149,8 @@ int main(int argc, char* argv[]) {
 	x.open(&xwin, 60, 40, scr.size.w-120, scr.size.h-80, "gtest", X_STYLE_NORMAL);
 	xwin.setVisible(true);
 
-	//pthread_create(NULL, NULL, loop, &xwin);
-	//x.run(do_thread, &xwin);
+	//pthread_create(NULL, NULL, do_thread, &xwin);
+	//x.run(NULL, &xwin);
 	x.run(loop, &xwin);
 	return 0;
 } 
