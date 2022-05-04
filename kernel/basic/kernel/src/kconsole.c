6a7933bc53be02af2a88a7a13ef68a6f766e8d02
misazhu <misa.zhu@gmail.com>
2022-05-05 05:40:53 +0800

    kernel console

--- kernel/basic/kernel/src/kconsole.c
+++ kernel/basic/kernel/src/kconsole.c
@@ -16,7 +16,7 @@ void kconsole_init(void) {
 		_fb_g = graph_new((uint32_t*)fbinfo.pointer, fbinfo.width, fbinfo.height);
 		graph_clear(_fb_g, 0xff000000);
 
-		_console.font = &font_8x16;
+		_console.font = get_font();
 		_console.fg_color = 0xffcccccc;
 		_console.bg_color = 0xff000000;
 		console_reset(&_console, _fb_g->w, _fb_g->h);
@@ -26,8 +26,8 @@ void kconsole_init(void) {
 void kconsole_input(const char* s) {
 	if(_fb_g == NULL)
 		return;
-	console_put_string(&_console, s);
-	console_refresh(&_console, _fb_g);
+	//console_put_string(&_console, s);
+	//console_refresh(&_console, _fb_g);
 }
 
 void kconsole_close(void) {
