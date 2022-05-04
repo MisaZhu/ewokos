150061797ca9a7496fe304c8af864c8c65ef0728
misazhu <misa.zhu@gmail.com>
2022-05-04 22:03:35 +0800

    reorg makefiles

--- system/basic/drivers/proc/state/build.mk
+++
@@ -1,9 +0,0 @@
-STATED_OBJS = $(ROOT_DIR)/drivers/proc/state/stated.o
-
-STATED = $(TARGET_DIR)/$(ROOT_DIR)/drivers/proc/stated
-
-PROGS += $(STATED)
-CLEAN += $(STATED_OBJS) $(STATED)
-
-$(STATED): $(STATED_OBJS) $(LIB_OBJS)
-	$(LD) -Ttext=100 $(STATED_OBJS) -o $(STATED) $(LDFLAGS) -lewokc -lc
--- system/basic/drivers/proc/sysinfo/build.mk
+++
@@ -1,9 +0,0 @@
-SYSINFOD_OBJS = $(ROOT_DIR)/drivers/proc/sysinfo/sysinfod.o
-
-SYSINFOD = $(TARGET_DIR)/$(ROOT_DIR)/drivers/proc/sysinfod
-
-PROGS += $(SYSINFOD)
-CLEAN += $(SYSINFOD_OBJS) $(SYSINFOD)
-
-$(SYSINFOD): $(SYSINFOD_OBJS) $(LIB_OBJS)
-	$(LD) -Ttext=100 $(SYSINFOD_OBJS) -o $(SYSINFOD) $(LDFLAGS) -lewokc -lc
