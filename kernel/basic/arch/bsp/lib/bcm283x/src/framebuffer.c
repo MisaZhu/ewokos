974dc265f82a5208c5f5e096054e9fe71368edec
May the source be with you <misa.zhu@gmail.com>
2022-05-05 16:16:45 +0800

    kernel framebuffer

--- kernel/basic/arch/bsp/lib/bcm283x/src/framebuffer.c
+++ kernel/basic/arch/bsp/lib/bcm283x/src/framebuffer.c
@@ -21,12 +21,10 @@ typedef struct {
 	uint32_t size;
 } fb_init_t;
 
-static __attribute__((__aligned__(PAGE_SIZE))) fb_init_t _fbinit;
 static fbinfo_t _fb_info;
 
 int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
-	fb_init_t* fbinit = &_fbinit;
-
+	fb_init_t* fbinit = (fb_init_t*)kalloc4k();
 	memset(&_fb_info, 0, sizeof(fbinfo_t));
 	memset(fbinit, 0, sizeof(fb_init_t));
 	
@@ -43,6 +41,10 @@ int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
 	//msg.data = ((uint32_t)&fbinit + 0xC0000000) >> 4;//gpu address add 0xc0000000 with l2 cache disabled.
 	mailbox_send(FRAMEBUFFER_CHANNEL, &msg);
 	mailbox_read(FRAMEBUFFER_CHANNEL, &msg);
+	if(fbinit->pointer == 0) {
+		kfree4k(fbinit);
+		return -1;
+	}
 
 	_fb_info.width = fbinit->width;
 	_fb_info.height = fbinit->height;
@@ -57,6 +59,8 @@ int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
 	_fb_info.xoffset = 0;
 	_fb_info.yoffset = 0;
 	_fb_info.size_max = _sys_info.phy_mem_size - (_fb_info.pointer-KERNEL_BASE);
+
+	kfree4k(fbinit);
 	map_pages(_kernel_vm, 
 		_fb_info.pointer,
 		V2P(_fb_info.pointer),
