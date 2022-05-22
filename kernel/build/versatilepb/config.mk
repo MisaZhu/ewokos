b3801f697e633a1520721075d28eb5def2022a0d
May the source be with you <misa.zhu@gmail.com>
2022-05-23 01:23:40 +0800

    update

--- kernel/build/versatilepb/config.mk
+++ kernel/build/versatilepb/config.mk
@@ -1,6 +1,10 @@
 CPU = arm926ej-s
 #QEMU_FLAGS = -cpu arm926 -M versatilepb -m 256M -nographic -display none -serial mon:stdio
 QEMU_FLAGS = -cpu arm926 -M versatilepb -m 256M -serial mon:stdio
-ARCH_CFLAGS = -mcpu=$(CPU) -DKCONSOLE
+ARCH_CFLAGS = -mcpu=$(CPU)
 ARCH=arm/v6
 BSP=versatilepb
+
+#----kernel console-------
+KERNEL_CONSOLE=yes
+
