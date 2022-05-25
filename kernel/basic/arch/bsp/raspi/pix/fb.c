312f5679f145f0d8155ae5d14baefb1c1ce31e07
misazhu <misa.zhu@gmail.com>
2022-05-25 18:47:01 +0800

    framebuffer 16bits

--- kernel/basic/arch/bsp/raspi/pix/fb.c
+++ kernel/basic/arch/bsp/raspi/pix/fb.c
@@ -1,16 +1,20 @@
 #include <dev/fb.h>
 #include <bcm283x/framebuffer.h>
 #include <kstring.h>
+#include <stddef.h>
 
+static uint16_t* _g16 = NULL;
 int32_t fb_init(uint32_t w, uint32_t h, fbinfo_t* fbinfo) {
+	_g16 = NULL;
 	if(bcm283x_fb_init(w, h, 32) != 0)
 		return -1;
 	memcpy(fbinfo, bcm283x_get_fbinfo(), sizeof(fbinfo_t));
+	if(fbinfo->depth == 16)
+		_g16 = (uint16_t*)fbinfo->pointer;
 	return 0;
 }
 
 void fb_flush32(uint32_t* g32, uint32_t w, uint32_t h) {
-	(void)g32;
-	(void)w;
-	(void)h;
+	if(_g16 != NULL)
+		blt16(g32, _g16, w, h);
 }
