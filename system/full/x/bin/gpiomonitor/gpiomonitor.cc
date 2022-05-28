2483270bb8b181e4f591881259ba9a01031c093c
May the source be with you <misa.zhu@gmail.com>
2022-05-29 01:57:32 +0800

    draw gpio

--- system/full/x/bin/gpiomonitor/gpiomonitor.cc
+++ system/full/x/bin/gpiomonitor/gpiomonitor.cc
@@ -20,32 +20,44 @@ class GPIOMonitorX : public XWin {
 	uint32_t gpioNum;
 	int cols;
 	font_t*  font;
+	static const int margin = 8;
 public:
 	inline GPIOMonitorX() {
 		font = font_by_name("8x16");
 		gpioNum = 40;	
 		cols = 10;
 	}
+
+	void drawGPIO(graph_t* g, int x, int y, int w, int h, int gpio) {
+		graph_box(g, x, y, w, h, 0xff000000);
+
+		char s[8];
+		snprintf(s, 7, "%d", gpio+1);
+
+		int r = font->h/2;
+		x += 4;
+		y += h/2 - r;
+
+		graph_draw_text(g, x, y, s, font, 0xff000000);
+		if(bcm283x_gpio_read(gpio) == 0)
+			graph_fill_circle(g, x+r+font->w*2, y+r, r, 0xff00ff00);
+		else
+			graph_fill_circle(g, x+r+font->w*2, y+r, r, 0xffff0000);
+	}
+
 protected:
 	void onRepaint(graph_t* g) {
 		graph_clear(g, 0xffffffff);
-		char s[8];
 
 		int rows = gpioNum / cols;
-		int w = (g->w-16) / cols;
-		int h = (g->h-16) / rows;
+		int w = (g->w - margin*2) / cols;
+		int h = (g->h - margin*2) / rows;
 		for(int j=0; j<rows; j++) {
 			for(int i=0; i<cols; i++) {
 				int gpio = j*cols + i;
-				snprintf(s, 7, "%d", gpio+1);
-				int r = font->h/2;
-				int x = i*w + w/2 - r;
-				int y = j*h + h/2 - r;
-				graph_draw_text(g, x, y, s, font, 0xff000000);
-				if(bcm283x_gpio_read(gpio) == 0)
-					graph_fill_circle(g, x+r+font->w*2, y+r, r, 0xff00ff00);
-				else
-					graph_fill_circle(g, x+r+font->w*2, y+r, r, 0xffff0000);
+				int x = i*w + margin;
+				int y = j*h + margin;
+				drawGPIO(g, x, y, w, h, gpio);
 			}
 		}
 	}
