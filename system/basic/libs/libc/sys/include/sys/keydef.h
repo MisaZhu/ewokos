8924f4e24095b02663b87fa2ca8af19c94b0b412
misazhu <misa.zhu@gmail.com>
2022-05-30 23:01:44 +0800

    update

--- system/basic/libs/libc/sys/include/sys/keydef.h
+++ system/basic/libs/libc/sys/include/sys/keydef.h
@@ -5,31 +5,31 @@
 extern "C" {
 #endif
 
-static const int KEY_HOME      = 1;
-static const int KEY_UP        = 5;
-static const int KEY_DOWN      = 24;
-static const int KEY_LEFT      = 19;
-static const int KEY_RIGHT     = 4;
-static const int KEY_ESC       = 27;
-static const int KEY_ENTER     = 13;
-static const int KEY_BACKSPACE = 127;
-static const int KEY_POWER     = 26;
-static const int CONSOLE_LEFT  = 8;
-static const int KEY_BUTTON_A = 96;
-static const int KEY_BUTTON_B = 97;
-static const int KEY_BUTTON_C = 98;
-static const int KEY_BUTTON_L1 = 102;
-static const int KEY_BUTTON_L2 = 104;
-static const int KEY_BUTTON_MODE = 110;
-static const int KEY_BUTTON_R1 = 103;
-static const int KEY_BUTTON_R2 = 105;
-static const int KEY_BUTTON_SELECT = 109;
-static const int KEY_BUTTON_START = 108;
-static const int KEY_BUTTON_THUMBL = 106;
-static const int KEY_BUTTON_THUMBR = 107;
-static const int KEY_BUTTON_X = 99;
-static const int KEY_BUTTON_Y = 100;
-static const int KEY_BUTTON_Z = 101;
+#define KEY_HOME            1
+#define KEY_UP              5
+#define KEY_DOWN            24
+#define KEY_LEFT            19
+#define KEY_RIGHT           4
+#define KEY_ESC             27
+#define KEY_ENTER           13
+#define KEY_BACKSPACE       127
+#define KEY_POWER           26
+#define CONSOLE_LEFT        8
+#define KEY_BUTTON_A        96
+#define KEY_BUTTON_B        97
+#define KEY_BUTTON_C        98
+#define KEY_BUTTON_L1       102
+#define KEY_BUTTON_L2       104
+#define KEY_BUTTON_MODE     110
+#define KEY_BUTTON_R1       103
+#define KEY_BUTTON_R2       105
+#define KEY_BUTTON_SELECT   109
+#define KEY_BUTTON_START    108
+#define KEY_BUTTON_THUMBL   106
+#define KEY_BUTTON_THUMBR   107
+#define KEY_BUTTON_X        99
+#define KEY_BUTTON_Y        100
+#define KEY_BUTTON_Z        101
 
 #ifdef __cplusplus
 }
