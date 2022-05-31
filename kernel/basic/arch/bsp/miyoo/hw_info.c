f610dc0b623ec9c35b211a20ed2cd027471163d4
May the source be with you <misa.zhu@gmail.com>
2022-06-01 01:31:14 +0800

    miyoo smp

--- kernel/basic/arch/bsp/miyoo/hw_info.c
+++ kernel/basic/arch/bsp/miyoo/hw_info.c
@@ -41,13 +41,7 @@ extern char __entry[];
 inline void __attribute__((optimize("O0"))) start_core(uint32_t core_id) { //TODO
     if(core_id >= _sys_info.cores)
         return;
-	//uint32_t entry = (uint32_t)(__entry);
-	//uint32_t entry = (uint32_t)(__entry) + _sys_info.kernel_base;
 	uint32_t entry = V2P((uint32_t)(__entry) + _sys_info.kernel_base);
-	printf("entry: 0x%x\n", entry);
-	do {
- 	   put32(SECOND_MAGIC_NUMBER_ADDR, 0xBABE);
-	} while(get32(SECOND_MAGIC_NUMBER_ADDR) != 0XBABE);
 
 	do {
  	   put32(SECOND_START_ADDR_HI, (entry >> 16));
@@ -56,6 +50,10 @@ inline void __attribute__((optimize("O0"))) start_core(uint32_t core_id) { //TOD
 	do {
    		put32(SECOND_START_ADDR_LO, (entry & 0xffff));
 	} while(get32(SECOND_START_ADDR_LO) != (entry & 0xffff));
+
+	do {
+ 	   put32(SECOND_MAGIC_NUMBER_ADDR, 0xBABE);
+	} while(get32(SECOND_MAGIC_NUMBER_ADDR) != 0XBABE);
     __asm__("sev");
 }
 #endif
