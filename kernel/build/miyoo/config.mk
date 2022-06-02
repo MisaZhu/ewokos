e4c4935ec870527875c240ace7a1726521807b0c
misazhu <misa.zhu@gmail.com>
2022-06-02 23:49:39 +0800

    update

--- kernel/build/miyoo/config.mk
+++ kernel/build/miyoo/config.mk
@@ -1,6 +1,6 @@
 CPU = cortex-a7
 QEMU_FLAGS = -cpu $(CPU) -M raspi2b -m 128M -serial mon:stdio
-ARCH_CFLAGS = -march=armv7ve -DSKIP_HYP -DDUAL_CORE
+ARCH_CFLAGS = -march=armv7ve -DDUAL_CORE
 ARCH=arm/v7
 BSP=miyoo
 
