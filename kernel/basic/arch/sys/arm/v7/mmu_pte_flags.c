72c16c6670387e2afdc7e8c35bf274ef862d9d58
May the source be with you <misa.zhu@gmail.com>
2022-06-05 15:19:33 +0800

    pte_attr

--- kernel/basic/arch/sys/arm/v7/mmu_pte_flags.c
+++ kernel/basic/arch/sys/arm/v7/mmu_pte_flags.c
@@ -11,7 +11,7 @@ inline void set_pte_flags(page_table_entry_t* pte, uint32_t pte_attr) {
 	}
 	else if(pte_attr == PTE_ATTR_KIMG) { //kernel image mem
 		pte->tex = 0x1;
-		pte->cacheable = 0;
+		pte->cacheable = 1;
 		pte->bufferable = 1;
 	}
 	else if(pte_attr == PTE_ATTR_WRTHR) { //write throuh mem
