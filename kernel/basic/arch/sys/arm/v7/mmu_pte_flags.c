1c2bd1544ccd1e094979543ae4da3426be0d56ee
May the source be with you <misa.zhu@gmail.com>
2022-06-09 03:22:02 +0800

    update

--- kernel/basic/arch/sys/arm/v7/mmu_pte_flags.c
+++ kernel/basic/arch/sys/arm/v7/mmu_pte_flags.c
@@ -9,7 +9,7 @@ inline void set_pte_flags(page_table_entry_t* pte, uint32_t pte_attr) {
 		pte->cacheable = 1;
 		pte->bufferable = 1;
 	}
-	else if(pte_attr == PTE_ATTR_WRBACK_ALLOCATE) { //kernel image mem
+	else if(pte_attr == PTE_ATTR_WRBACK_ALLOCATE) { //write back allocate mem
 		pte->tex = 0x1;
 		pte->cacheable = 1;
 		pte->bufferable = 1;
