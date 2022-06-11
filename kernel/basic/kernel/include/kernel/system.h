9ec0a2632a8339b42c0d5c907e5212bd9d785362
May the source be with you <misa.zhu@gmail.com>
2022-06-11 23:57:34 +0800

    update

--- kernel/basic/kernel/include/kernel/system.h
+++ kernel/basic/kernel/include/kernel/system.h
@@ -8,7 +8,6 @@ extern void __irq_disable(void);
 
 extern uint32_t __int_off(void);
 extern void __int_on(uint32_t);
-extern void __enable_scu(void);
 
 extern void _delay(uint32_t count);
 extern void _delay_usec(uint64_t count);
