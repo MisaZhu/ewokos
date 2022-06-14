64fb103c94ab855590d0c9e7695a4cf4be9200ac
May the source be with you <misa.zhu@gmail.com>
2022-06-14 23:43:21 +0800

    finder icons

--- system/full/x/bin/finder/finder.cc
+++ system/full/x/bin/finder/finder.cc
@@ -14,6 +14,15 @@ using namespace Ewok;
 
 class Finder: public XWin {
 	font_t* font;
+	uint32_t bgColor;
+	uint32_t fgColor;
+	uint32_t hideColor;
+	uint32_t selectColor;
+	uint32_t titleColor;
+	graph_t* dirIcon;
+	graph_t* fileIcon;
+	uint32_t itemSize;
+
 	int     selected;
 	int     start;
 	static const int MAX_FILES = 256;
@@ -112,32 +121,50 @@ class Finder: public XWin {
 protected:
 	void onRepaint(graph_t* g) {
 		char name[FS_FULL_NAME_MAX+1];
-		graph_clear(g, 0xff000000);
-		int h = font->h + 4;
+		int h = itemSize;
+		int yMargin = (itemSize - font->h)/2;
+		int xMargin = 8;
+
+		graph_clear(g, bgColor);
 		snprintf(name, FS_FULL_NAME_MAX, "[%s]", cwd);
-		graph_draw_text(g, 8, 4, name, font, 0xffffff00);
+		if(dirIcon != NULL) {
+			int iconMargin = (itemSize - dirIcon->h)/2;
+			graph_blt_alpha(dirIcon, 0, 0, dirIcon->w, dirIcon->h,
+					g, xMargin, iconMargin, dirIcon->w, dirIcon->h, 0xff);
+			xMargin += dirIcon->w + 4;
+		}
+		graph_draw_text(g, xMargin, yMargin, name, font, titleColor);
 
 		for(int i=start; i<nums; i++) {
 			struct dirent* it = &files[i];
 			if(i == selected)
-				graph_fill(g, 2, (i+1-start)*h, g->w-2, h, 0xff444444);
+				graph_fill(g, 2, (i+1-start)*h, g->w-2, h, selectColor);
 
-			uint32_t color = 0xffffffff;
+			uint32_t color = fgColor;
 			if(it->d_name[0] == '.')
-				color = 0xff888888;
+				color = hideColor;
 
+			graph_t* icon = NULL;
 			if(it->d_type == DT_DIR)
-				snprintf(name, FS_FULL_NAME_MAX, "[%s]", it->d_name);
+				icon = dirIcon;
 			else
-				strcpy(name, it->d_name);
-			graph_draw_text(g, 8, (i+1-start)*h+4, name, font, color);
+				icon = fileIcon;
+
+			xMargin = 8;
+			if(icon != NULL) {
+				int iconMargin = (itemSize - dirIcon->h)/2;
+				graph_blt_alpha(icon, 0, 0, icon->w, icon->h,
+						g, xMargin, (i+1-start)*h+iconMargin, icon->w, icon->h, 0xff);
+				xMargin += icon->w + 4;
+			}
+			graph_draw_text(g, xMargin, (i+1-start)*h+yMargin, it->d_name, font, color);
 		}
 	}
 
 	void onEvent(xevent_t* ev) {
 		xinfo_t xinfo;
 		getInfo(xinfo);
-		int h = font->h + 4;
+		int h = itemSize;
 		int lines = xinfo.wsr.h/h - 1;
 		
 		if(ev->type == XEVT_IM) {
@@ -178,6 +205,15 @@ protected:
 	}
 public:
 	inline Finder() {
+		bgColor = 0xff000000;
+		fgColor = 0xffffffff;
+		hideColor = 0xff888888;
+		selectColor = 0xff444444;
+		titleColor = 0xffffff00;
+		fileIcon = png_image_new("/data/icons/system/32/file.png");
+		dirIcon = png_image_new("/data/icons/system/32/dir.png");
+		itemSize = 36;
+
 		selected = 0;
 		start = 0;
 		font = font_by_name("8x16");
@@ -185,13 +221,45 @@ public:
 	}
 
 	inline ~Finder() {
+		if(fileIcon != NULL)
+			graph_free(fileIcon);
+		if(dirIcon != NULL)
+			graph_free(dirIcon);
 	}
 
 	bool readConfig(const char* fname) {
 		sconf_t *conf = sconf_load(fname);	
 		if(conf == NULL)
 			return false;
-		font = font_by_name(sconf_get(conf, "font"));
+
+		const char* v = sconf_get(conf, "font");
+		if(v[0] != 0)
+			font = font_by_name(v);
+		v = sconf_get(conf, "item_size");
+		if(v[0] != 0)
+			itemSize = atoi(v);
+		v = sconf_get(conf, "bg_color");
+		if(v[0] != 0)
+			bgColor = atoi_base(v, 16);
+		v = sconf_get(conf, "fg_color");
+		if(v[0] != 0)
+			fgColor = atoi_base(v, 16);
+		v = sconf_get(conf, "select_color");
+		if(v[0] != 0)
+			selectColor = atoi_base(v, 16);
+		v = sconf_get(conf, "hide_color");
+		if(v[0] != 0)
+			hideColor = atoi_base(v, 16);
+		v = sconf_get(conf, "title_color");
+		if(v[0] != 0)
+			titleColor = atoi_base(v, 16);
+		v = sconf_get(conf, "file_icon");
+		if(v[0] != 0)
+			fileIcon = png_image_new(v);
+		v = sconf_get(conf, "dir_icon");
+		if(v[0] != 0)
+			dirIcon = png_image_new(v);
+
 		sconf_free(conf);
 		return true;
 	}
