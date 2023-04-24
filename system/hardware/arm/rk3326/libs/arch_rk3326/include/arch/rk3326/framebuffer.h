8f1cc1d81f3d96c94e1c1e8fd9758807620e69c9
misazhu <misa.zhu@gmail.com>
2023-04-24 18:29:15 +0800

    rk3326 removed

--- system/hardware/arm/rk3326/libs/arch_rk3326/include/arch/rk3326/framebuffer.h
+++
@@ -1,9 +0,0 @@
-#ifndef RK3326_FRAMEBUFFER_H
-#define RK3326_FRAMEBUFFER_H
-
-#include <sys/fbinfo.h>
-
-fbinfo_t* rk3326_get_fbinfo(void);
-int32_t rk3326_fb_init(uint32_t w, uint32_t h, uint32_t dep);
-
-#endif
