255a4340f4cc5a378edd1de858532ddcec9d46a4
misazhu <misa.zhu@gmail.com>
2023-08-11 20:37:33 +0800

    timer test

--- system/basic/bin/test/test.c
+++ system/basic/bin/test/test.c
@@ -8,6 +8,7 @@
 static uint32_t _v;
 
 static void timer_handle(void) {
+	printf("timer test: pid: %d, v: %d\n", getpid(), _v);
 	_v++;
 }
 
@@ -19,12 +20,11 @@ int main(int argc, char* argv[]) {
 	uint32_t id = timer_set(1000000, timer_handle);
 
 	while(1) {
-		printf("pid: %d, v: %d\n", getpid(), _v);
-		sleep(1);
-
+		usleep(50000);
 		if(_v > 10)  {
-			timer_remove(id);
+			break;
 		}
 	}
+	timer_remove(id);
 	return 0;
 }
