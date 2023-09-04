627181fa5d06ca868ff1a69ce5799d679409c304
tony <tao.zeng@rokid.com>
2023-09-04 18:15:35 +0800

    fix build error

--- system/full/drivers/xconsoled/xmake.lua
+++ system/full/drivers/xconsoled/xmake.lua
@@ -1,6 +1,6 @@
 target("xconsoled")
     set_type("application")
-    add_deps("libgraph","libttf","libfont","libhash", "libupng","libx++", "libx","libsconf", "libcxx")
+    add_deps("libgraph","libconsole", "libttf","libfont","libhash", "libupng","libx++", "libx","libsconf", "libcxx")
     add_files("**.cc")        
     install_dir("drivers")
 target_end()
