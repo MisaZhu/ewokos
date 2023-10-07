f6c1845c95b3c15b238400ef7c7851e997d652aa
misazhu <misa.zhu@gmail.com>
2023-10-08 00:01:13 +0800

    neon

--- system/full/x/drivers/xserverd/xserverd.c
+++ system/full/x/drivers/xserverd/xserverd.c
@@ -221,6 +221,7 @@ static int draw_win(x_t* xp, xwin_t* win) {
 	uint32_t to = 0;
 	graph_t* g = win->g_buf;
 	if(g != NULL) {
+#ifdef NEON_ENABLE
 		if((win->xinfo->style & X_STYLE_ALPHA) != 0) {
 			graph_blt_alpha_neon(g, 0, 0, 
 					win->xinfo->wsr.w,
@@ -230,6 +231,7 @@ static int draw_win(x_t* xp, xwin_t* win) {
 					win->xinfo->wsr.y,
 					win->xinfo->wsr.w,
 					win->xinfo->wsr.h, 0xff);
+
 		}
 		else {
 			graph_blt_neon(g, 0, 0, 
@@ -241,6 +243,29 @@ static int draw_win(x_t* xp, xwin_t* win) {
 						win->xinfo->wsr.w,
 						win->xinfo->wsr.h);
 		}
+#else
+		if((win->xinfo->style & X_STYLE_ALPHA) != 0) {
+			graph_blt_alpha(g, 0, 0, 
+					win->xinfo->wsr.w,
+					win->xinfo->wsr.h,
+					display->g,
+					win->xinfo->wsr.x,
+					win->xinfo->wsr.y,
+					win->xinfo->wsr.w,
+					win->xinfo->wsr.h, 0xff);
+
+		}
+		else {
+			graph_blt(g, 0, 0, 
+						win->xinfo->wsr.w,
+						win->xinfo->wsr.h,
+						display->g,
+						win->xinfo->wsr.x,
+						win->xinfo->wsr.y,
+						win->xinfo->wsr.w,
+						win->xinfo->wsr.h);
+		}
+#endif
 	}
 
 	draw_win_frame(xp, win);
