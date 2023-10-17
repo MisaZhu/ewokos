0899f8826ff02f888ac6297b0285c16c8c0248d2
May the source be with you <misa.zhu@gmail.com>
2023-10-18 00:42:34 +0800

    update

--- system/full/libs/fbd/xmake.lua
+++ system/full/libs/fbd/xmake.lua
@@ -1,6 +1,6 @@
 target("libfbd")
     set_type("library")
     add_files("**.c")        
-    add_deps("libfb", "libupng")
+    add_deps("libfb", "libsconf", "libupng")
     add_includedirs("include",  {public = true})
 target_end()
