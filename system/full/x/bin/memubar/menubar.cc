8139f32326c891946a91f3bc74909c4121fd2234
May the source be with you <misa.zhu@gmail.com>
2023-11-01 03:06:06 +0800

    update

--- system/full/x/bin/memubar/menubar.cc
+++ system/full/x/bin/memubar/menubar.cc
@@ -4,21 +4,33 @@
 #include <string.h>
 #include <sys/keydef.h>
 #include <upng/upng.h>
+#include <font/font.h>
 #include <x++/X.h>
 
 using namespace Ewok;
 
 class Menubar : public XWin {
+	graph_t* logo;
+	font_t* font;
 protected:
 	void onRepaint(graph_t* g) {
 		graph_fill(g, 0, 0, g->w, g->h, 0xffffffff);
+		graph_blt_alpha(logo, 0, 0, logo->w, logo->h,
+				g, 10, (g->h-logo->h)/2, logo->w, logo->h, 0xff);
+		graph_draw_text_font(g, 20 + logo->w, (g->h - font->max_size.y)/2, "EwokOS", font, 0xff000000);
 	}
 
 public:
 	Menubar() {
+		logo = png_image_new("/usr/system/icons/os.png");
+		font = font_new(DEFAULT_SYSTEM_FONT, 12, true);
 	}
 
 	~Menubar() {
+		if(logo != NULL)
+			graph_free(logo);
+		if(font != NULL)
+			font_free(font);
 	}
 };
 
@@ -27,14 +39,14 @@ int main(int argc, char* argv[]) {
 	xscreen_t scr;
 	x.getScreenInfo(scr, 0);
 	Menubar xwin;
-	x.open(&xwin, 0, 0, scr.size.w, 32, "menubar",
+	x.open(&xwin, 0, 0, scr.size.w, 28, "menubar",
 			XWIN_STYLE_NO_FRAME | XWIN_STYLE_NO_FOCUS);
 	xwin.setVisible(true);
 
 	grect_t desk;
 	x.getDesktopSpace(desk, 0);
-	desk.y += 32;
-	desk.h -= 32;
+	desk.y += 28;
+	desk.h -= 28;
 	x.setDesktopSpace(desk);
 
 	x.run(NULL);
