964e88f42d2623c0b96add44553361baabf93e21
misazhu <misa.zhu@gmail.com>
2023-11-03 14:27:34 +0800

    clockwork kernel porting

--- kernel/hardware/arm/raspi/lib/bcm283x/src/framebuffer.c
+++ kernel/hardware/arm/raspi/lib/bcm283x/src/framebuffer.c
@@ -25,7 +25,21 @@ typedef struct {
 
 static __attribute__((__aligned__(PAGE_SIZE))) fb_init_t _fbinit;
 int32_t fb_init_raw(uint32_t w, uint32_t h, uint32_t dep) {
-#if 0
+
+#if CLOCKWORK
+	_fb_info.width = 480;
+	_fb_info.height = 1280;
+	_fb_info.vwidth = 480;
+	_fb_info.vheight = 1280;
+	_fb_info.depth = 16;
+	_fb_info.pitch = _fb_info.width*(_fb_info.depth/8);
+
+	_fb_info.pointer = P2V(0xC00000); //GPU addr to ARM addr
+	_fb_info.size = 480*1280*2;
+	_fb_info.xoffset = 0;
+	_fb_info.yoffset = 0;
+	_fb_info.size_max = 480*1280*2;
+#else
 	fb_init_t* fbinit = &_fbinit;
 	//fb_init_t* fbinit = (fb_init_t*)kalloc4k();
 	memset(&_fb_info, 0, sizeof(fbinfo_t));
@@ -61,19 +75,6 @@ int32_t fb_init_raw(uint32_t w, uint32_t h, uint32_t dep) {
 	_fb_info.xoffset = 0;
 	_fb_info.yoffset = 0;
 	_fb_info.size_max = _sys_info.phy_mem_size - (_fb_info.pointer-_sys_info.kernel_base);
-#else 
-	_fb_info.width = 480;
-	_fb_info.height = 1280;
-	_fb_info.vwidth = 480;
-	_fb_info.vheight = 1280;
-	_fb_info.depth = 16;
-	_fb_info.pitch = _fb_info.width*(_fb_info.depth/8);
-
-	_fb_info.pointer = P2V(0xC00000); //GPU addr to ARM addr
-	_fb_info.size = 480*1280*2;
-	_fb_info.xoffset = 0;
-	_fb_info.yoffset = 0;
-	_fb_info.size_max = 480*1280*2;
 #endif
 	//kfree4k(fbinit);
 	map_pages_size(_kernel_vm, 
--- kernel/hardware/arm/raspi/pix/config.mk
+++ kernel/hardware/arm/raspi/pix/config.mk
@@ -5,6 +5,8 @@ ARCH=v7
 
 #----PI4-------
 #PI4=yes
+#----PI4.clockwork-------
+#CLOCKWORK=yes
 #----enable DPI display---
 #DPI=yes
 #----multi core(SMP)------
@@ -14,6 +16,9 @@ KERNEL_CONSOLE=yes
 
 ifeq ($(PI4),yes)
 ARCH_CFLAGS += -DPI4
+QEMU_FLAGS = -M raspi4 -m 1024M -serial mon:stdio
+else if($(CLOCKWORK),yes)
+ARCH_CFLAGS += -DPI4 -DCLOCKWORK
 QEMU_FLAGS = -M raspi4 -m 1024M -serial mon:stdio
 else
 QEMU_FLAGS = -M raspi2b -m 1024M -serial mon:stdio
