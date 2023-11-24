00d4dd7a4a23275c50e0d33da5c2ead321d79e22
May the source be with you <misa.zhu@gmail.com>
2023-11-24 22:52:34 +0800

    interrupt ignore when doing ipc

--- kernel/kernel/src/interrupt.c
+++ kernel/kernel/src/interrupt.c
@@ -86,6 +86,10 @@ static int32_t interrupt_send_raw(context_t* ctx, uint32_t interrupt,  interrupt
 			proc->ipc_res.state != IPC_IDLE)
 		return -1;
 
+	ipc_task_t* ipc = proc_ipc_get_task(proc);
+	if(ipc != NULL && ipc->state == IPC_BUSY)
+		return -1;
+
 	proc_save_state(proc, &proc->space->interrupt.saved_state);
 	proc->space->interrupt.interrupt = interrupt;
 	proc->space->interrupt.entry = intr->entry;
