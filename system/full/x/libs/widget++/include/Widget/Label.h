37f4a1acca76b42d70824b84a54fb98567d35aa4
May the source be with you <misa.zhu@gmail.com>
2023-11-30 04:34:39 +0800

    update

--- system/full/x/libs/widget++/include/Widget/Label.h
+++ system/full/x/libs/widget++/include/Widget/Label.h
@@ -3,43 +3,42 @@
 
 #include <Widget/Widget.h>
 #include <upng/upng.h>
-#include <sys/mstr.h>
+#include <string>
 #include <font/font.h>
 
+using namespace EwokSTL;
 namespace Ewok {
 
 class Label: public Widget {
 	font_t* font;
-	str_t* label;
+	string label;
 protected:
 	void onRepaint(graph_t* g, grect_t* rect) {
 		Widget::onRepaint(g, rect);
-		if(font == NULL || label == NULL)
+		if(font == NULL)
 			return;
 		graph_draw_text_font_align(g, rect->x+marginH, rect->y+marginV, rect->w-marginH*2, rect->h-marginV*2,
-					label->cstr, font, fgColor, FONT_ALIGN_CENTER);
+					label.c_str(), font, fgColor, FONT_ALIGN_CENTER);
 	}
 
 public:
 	Label(font_t* font, const char* str) {
 		this->font = font;
-		label = str_new(str);
+		label = str;
 	}
 
 	~Label(void) {
-		if(label != NULL)
-			str_free(label);
 	}
 
 	void setLabel(const char* str) {
-		str_cpy(label, str);
+		label = str;
 		update();
 	}
 
 	gsize_t getMinSize(void) {
 		gsize_t sz = { 0, 0 };
-		if(font != NULL && label != NULL)
-			font_text_size(label->cstr, font, (uint32_t*)&sz.w, (uint32_t*)&sz.h);
+		if(font != NULL)
+			font_text_size(label.c_str(), font, (uint32_t*)&sz.w, (uint32_t*)&sz.h);
 		return sz;
 	}
 };
