495c8d30d350a6bd54dfd30bcc4b19c0c384607c
May the source be with you <misa.zhu@gmail.com>
2023-12-25 00:19:23 +0800

    widget test

--- system/full/apps/wtest/wtest.cc
+++ system/full/apps/wtest/wtest.cc
@@ -25,20 +25,27 @@ public:
 class Anim: public Widget {
 	graph_t* img;
 	uint32_t step;
+	uint32_t pos;
 	int32_t steps;
 protected:
 	void onRepaint(graph_t* g, const Theme* theme, const grect_t& r) {
 		if(img == NULL)
 			return;
-		graph_fill(g, r.x, r.y, r.w, r.h, theme->bgColor);
+		//graph_fill(g, r.x, r.y, r.w, r.h, 0xff00ff00);
+		graph_gradation(g, r.x, r.y, r.w, r.h, 0xffffffff, 0xff00ff00, true);
 		graph_blt_alpha(img, step*(img->w/steps), 0, img->w/steps, img->h,
-				g, r.x+(r.w-img->w/steps)/2, r.y+(r.h-img->h)/2, img->w/steps, img->h, 0xff);
+				//g, r.x+(r.w-img->w/steps)/2, r.y+(r.h-img->h)/2, img->w/steps, img->h, 0xff);
+				g, r.x+pos, r.y+(r.h-img->h)/2, img->w/steps, img->h, 0xff);
+
+		if(pos > r.w)
+			pos = 0;
 	}
 
 	void onTimer() {
 		step++;
 		if (step >= steps)
 			step = 0;
+		pos += img->w/steps/steps;
 		update();
 	}
 
@@ -47,6 +54,7 @@ public:
 		step = 0;
 		img = png_image_new(X::getResName("data/walk.png"));
 		steps = 8;
+		pos = 0;
 	}
 
 	~Anim() {
@@ -58,30 +66,25 @@ public:
 int main(int argc, char** argv) {
 	X x;
 	WidgetWin win;
-	win.setRoot(new RootWidget());
-	win.getRoot()->setAlpha(false);
+	RootWidget* root = new RootWidget();
+	win.setRoot(root);
+	root->setAlpha(false);
 
-	Container* c = new Container();
-	c->setType(Container::HORIZONTAL);
-	win.getRoot()->add(c);
-
-	Widget* wd = new Image("/usr/system/icons/ewok.png");
-	c->add(wd);
-
-	wd = new Anim();
-	c->add(wd);
+	Widget* wd = new Anim();
+	wd->fix(0, 100);
+	root->add(wd);
 
 	Text* txt = new Text("text\nHello world\n[中文测试]\n123～！@");
 	Theme* theme = new Theme(font_new("/usr/system/fonts/system_cn.ttf", 18, true));
 	theme->bgColor = 0xff000000;
 	theme->fgColor = 0xffffaa88;
 	txt->setTheme(theme);
-	win.getRoot()->add(txt);
+	root->add(txt);
 
-	c = new Container();
+	Container* c = new Container();
 	c->setType(Container::HORIZONTAL);
 	c->fix(0, 40);
-	win.getRoot()->add(c);
+	root->add(c);
 
 	wd = new MyButton("test");
 	c->add(wd);
@@ -90,11 +93,11 @@ int main(int argc, char** argv) {
 	wd->disable();
 	c->add(wd);
 
-	win.getRoot()->setType(Container::VERTICLE);
+	root->setType(Container::VERTICLE);
 
 	x.open(0, &win, 400, 300, "widgetTest", XWIN_STYLE_NORMAL);
 	win.setVisible(true);
-	win.setTimer(10);
+	win.setTimer(12);
 	x.run(NULL, &win);
 	return 0;
 }
