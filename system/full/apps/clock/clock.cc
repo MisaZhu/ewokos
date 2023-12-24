ff4267fb662f9f88ef284477928c0d4798ac0e75
May the source be with you <misa.zhu@gmail.com>
2023-12-25 02:13:37 +0800

    clock style

--- system/full/apps/clock/clock.cc
+++ system/full/apps/clock/clock.cc
@@ -7,32 +7,50 @@
 
 using namespace Ewok;
 
-class Timer: public Label {
-protected:
-	void onRepaint(graph_t* g, const Theme* theme, const grect_t& r) {
+class Timer: public Widget {
+	uint32_t sec;
+	uint32_t min;
+	uint32_t hour;
+
+	void drawClock(graph_t* g, const Theme* theme, const grect_t& r, uint32_t clock) {
 		graph_gradation(g, r.x, r.y, r.w, r.h/2, 0xffffffff, 0xffaaaaaa, true);
 		graph_gradation(g, r.x, r.y+r.h/2, r.w, r.h/2, 0xffffffff, 0xffaaaaaa, true);
 
-		Label::onRepaint(g, theme, r);
+		char txt[4];
+		snprintf(txt, 3, "%02d", clock);
+		graph_draw_text_font_align(g, r.x, r.y, r.w, r.h,
+				txt, theme->font, theme->fgColor, FONT_ALIGN_CENTER);
 
 		graph_set(g, r.x, r.y+r.h/2-1, r.w, 2, 0x0);
 		graph_box(g, r.x, r.y, r.w, r.h/2-1, 0xff000000);
 		graph_box(g, r.x, r.y+r.h/2+1, r.w, r.h/2-1, 0xff000000);
 	}
 
+protected:
+
+	void onRepaint(graph_t* g, const Theme* theme, const grect_t& rect) {
+		grect_t r = {rect.x, rect.y, rect.w/3-2, rect.h};
+		drawClock(g, theme, r, hour);
+		r.x += (rect.w/3+1);
+		drawClock(g, theme, r, min);
+		r.x += (rect.w/3+1);
+		drawClock(g, theme, r, sec);
+	}
+
 	void onTimer() {
 		uint32_t ksec;
 		kernel_tic(&ksec, NULL);
-		uint32_t min = ksec / 60;
-		uint32_t hour = min / 60;
+		min = ksec / 60;
+		hour = min / 60;
 		min = min % 60;
-		ksec = ksec % 60;
-		char s[16];
-		snprintf(s, 15, "%02d:%02d:%02d", hour, min, ksec);
-		setLabel(s);
+		sec = ksec % 60;
+		update();
 	}
 public: 
-	Timer(const string& label = "00:00:00") : Label(label) {
+	Timer() {
+		sec = 0;
+		min = 0;
+		hour = 0;
 	}
 };
 
@@ -40,7 +58,7 @@ class ClockWin: public WidgetWin {
 protected:
 	void onRepaint(graph_t* g) {
 		setAlpha(true);
-		graph_clear(g, 0x88000000);
+		graph_clear(g, 0x0);
 		WidgetWin::onRepaint(g);
 	}
 
@@ -62,12 +80,13 @@ int main(int argc, char** argv) {
 	theme->bgColor = 0xffffffff;
 	theme->fgColor = 0xff000000;
 
-	win.setRoot(new RootWidget());
+	RootWidget* root = new RootWidget();
+	win.setRoot(root);
 	win.setTheme(theme);
-	win.getRoot()->setType(Container::HORIZONTAL);
+	root->setType(Container::HORIZONTAL);
 
 	Timer* timer = new Timer();
-	win.getRoot()->add(timer);
+	root->add(timer);
 
 	x.open(0, &win, 200, 68, "clock", XWIN_STYLE_NO_FRAME);
 	win.setVisible(true);
