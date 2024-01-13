42fccc8e3426fa8e93921ca72e883a7a15eef823
May the source be with you <misa.zhu@gmail.com>
2024-01-14 02:45:26 +0800

    update

--- system/basic/libs/libc/ewoksys/src/vdevice.c
+++ system/basic/libs/libc/ewoksys/src/vdevice.c
@@ -65,10 +65,10 @@ static void file_del(int fd, int pid, uint32_t node) {
 static fsinfo_t* file_get(int fd, int pid, uint32_t node) {
 	fsinfo_t* info = file_get_cache(fd, pid, node);
 	if(info == NULL) {
-		fsinfo_t i;
+		static fsinfo_t i;
 		if(vfs_get_by_node(node, &i) != 0)
 			return NULL;
-		info = file_add(fd, pid, &i);
+		info = &i;//file_add(fd, pid, &i);
 	}
 	return info;
 }
