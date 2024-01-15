09b710ce306738a825dfcd200a0348caa92b668e
May the source be with you <misa.zhu@gmail.com>
2024-01-16 01:24:43 +0800

    update

--- system/basic/libs/libc/src/stdio/fgetc.c
+++ system/basic/libs/libc/src/stdio/fgetc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <errno.h>
 
-int fgetc(FILE *fp)
-{
+int fgetc(FILE *fp) {
 	char c = 0;
-	fread( &c, 1, 1, fp);;
+	if(fread( &c, 1, 1, fp) != 1)
+		c = 0;
 	return c;
 }
