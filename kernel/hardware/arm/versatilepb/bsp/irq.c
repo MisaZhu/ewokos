c268bd74f60d86231dd718a9fd663cf589814eab
misazhu <misa.zhu@gmail.com>
2024-02-19 13:01:57 +0800

    update

--- kernel/hardware/arm/versatilepb/bsp/irq.c
+++ kernel/hardware/arm/versatilepb/bsp/irq.c
@@ -3,21 +3,11 @@
 #include <mm/mmu.h>
 #include "arch.h"
 
-#define UART0 ((volatile uint32_t*)(_sys_info.mmio.v_base+0x001f1000))
-/* serial port register offsets */
-#define UART_DATA        0x00
-#define UART_FLAGS       0x18
-#define UART_INT_ENABLE  0x0e
-#define UART_INT_TARGET  0x0f
-#define UART_INT_CLEAR   0x11
-
 /* memory mapping for the prime interrupt controller */
 #define PIC (_sys_info.mmio.v_base + 0x00140000)
 #define SIC (_sys_info.mmio.v_base + 0x00003000)
 
 #define PIC_INT_TIMER0 (1 << 4)
-#define PIC_INT_UART0  (1 << 12)
-
 
 void irq_arch_init(void) {
 }
