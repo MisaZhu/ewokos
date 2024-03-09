4f899d7c0ddfbde894af883c1bfca26bbc355050
misazhu <misa.zhu@gmail.com>
2024-03-10 04:12:03 +0800

    update

--- system/basic/libc/libewoksys/ewoksys/src/proc.c
+++ system/basic/libc/libewoksys/ewoksys/src/proc.c
@@ -51,8 +51,8 @@ static inline void proc_global_unlock(void) {
 		}
 	}
 		
-	_lock_thread = -1;
 	pthread_mutex_unlock(&_proc_global_lock);
+	_lock_thread = -1;
 }
 
 void __malloc_lock (struct _reent *reent) {
