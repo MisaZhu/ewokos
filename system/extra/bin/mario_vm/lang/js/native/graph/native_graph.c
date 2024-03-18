7fa120a6dc6d3cad96b6ba4516cea81745ec975d
misazhu <misa.zhu@gmail.com>
2024-03-18 18:09:51 +0800

    update

--- system/extra/bin/mario_vm/lang/js/native/graph/native_graph.c
+++ system/extra/bin/mario_vm/lang/js/native/graph/native_graph.c
@@ -1,12 +1,16 @@
 #include "native_graph.h"
 #include <ewoksys/klog.h>
 #include <graph/graph.h>
+#include <font/font.h>
 
 #define CLS_GRAPH "Graph"
+#define CLS_FONT "Font"
 
 #ifdef __cplusplus /* __cplusplus */
 extern "C" {
 #endif
+
+//===================Graph natives=================
 
 var_t* native_graph_clear(vm_t* vm, var_t* env, void* data) {
 	int color = get_int(env, "color");
@@ -64,12 +68,49 @@ var_t* native_graph_line(vm_t* vm, var_t* env, void* data) {
 	return NULL;
 }
 
+var_t* native_graph_drawText(vm_t* vm, var_t* env, void* data) {
+	int x = get_int(env, "x");
+	int y = get_int(env, "y");
+	const char* text = get_str(env, "text");
+	font_t* font = (font_t*)get_raw(env, "font");
+	int color = get_int(env, "color");
+
+	graph_t* g = (graph_t*)get_raw(env, THIS);
+	if(g == NULL || font == NULL)
+		return NULL;
+
+	klog("%d, %d, %s, font %x, %x\n", x, y, text, font, color);
+	graph_draw_text_font(g, x, y, text, font, color);
+	return NULL;
+}
+
+
+//===================Font natives=================
+static void free_font(void* p) {
+	font_t* font = (font_t*)p;
+	font_free(font);
+}
+
+var_t* native_font_constructor(vm_t* vm, var_t* env, void* data) {
+	const char* name = get_str(env, "name");
+	int size = get_int(env, "size");
+	font_t* font = font_new(name, size, true);
+
+	var_t* thisV = var_new_obj(vm, font, (free_func_t)free_font);
+	var_instance_from(thisV, get_obj(env, THIS));
+	return thisV;
+}
+
 void reg_native_graph(vm_t* vm) {
 	var_t* cls = vm_new_class(vm, CLS_GRAPH);
 	vm_reg_native(vm, cls, "clear(color)", native_graph_clear, NULL); 
 	vm_reg_native(vm, cls, "fill(x,y,w,h,color)", native_graph_fill, NULL); 
 	vm_reg_native(vm, cls, "box(x,y,w,h,color)", native_graph_box, NULL); 
 	vm_reg_native(vm, cls, "line(x0,y0,x1,y1,color)", native_graph_line, NULL); 
+	vm_reg_native(vm, cls, "drawText(x, y, text, font, color)", native_graph_drawText, NULL); 
+
+	cls = vm_new_class(vm, CLS_FONT);
+	vm_reg_native(vm, cls, "constructor(name, size)", native_font_constructor, NULL); 
 }
 
 #ifdef __cplusplus /* __cplusplus */
--- system/full/data/js/x.js
+++ system/full/data/js/x.js
@@ -5,6 +5,8 @@ xwin.i = 0;
 xwin.onRepaint = function(g) {
     g.clear(0xffffffff);
     g.fill(10, 10, 40, 40, 0xff000000 | i);
+    var font = new Font("/usr/system/fonts/system.ttf", 16);
+    g.drawText(10, 50, "Hello, World!", font, 0xffff0000);
 };
 
 
