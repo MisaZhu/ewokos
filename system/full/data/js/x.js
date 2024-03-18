93560cc8ae087ab5a35cb904ee896ba58f48c3b1
misazhu <misa.zhu@gmail.com>
2024-03-19 02:14:15 +0800

    update

--- system/full/data/js/x.js
+++ system/full/data/js/x.js
@@ -4,19 +4,28 @@ var png = PNG.load("/usr/system/images/mac1984.png");
 var font = new Font("/usr/system/fonts/system.ttf", 16);
 
 var mevent = null;
+var ievent = null;
+
 var ix = 0, iy = 0;
 
 xwin.onRepaint = function(g) {
     g.clear(0xffffffff);
-    if(mevent == null) {
-        g.drawText(0, 0, "try move and click mouse anywhere", font, 0xffff0000);
-        return;
-    }
-
     g.bltAlpha(png, 0, 0, png.width, png.height, ix, iy, png.width, png.height, 0xff);
-    g.drawText(0, 0, "mouse: " +
-            mevent.x + ", " + mevent.y +
-            ", state: " + mevent.state, font, 0xffff0000);
+
+    if(mevent == null)
+        g.drawText(0, 0, "try move and click mouse anywhere", font, 0xffff0000);
+    else 
+        g.drawText(0, 0, "mouse: " + 
+                mevent.x + ", " +  mevent.y +
+                ", state: " + mevent.state,
+                font, 0xffff0000);
+    
+    if(ievent == null)
+        g.drawText(0, 20, "try type any key", font, 0xffff0000);
+    else 
+        g.drawText(0, 20, "key: " + ievent.c +
+                ", state: " + ievent.state,
+                font, 0xffff0000);
 };
 
 xwin.onMouse = function(mouseEvt) {
@@ -31,6 +40,17 @@ xwin.onMouse = function(mouseEvt) {
 
 xwin.onIM = function(imEvt) {
     debug(imEvt);
+    ievent = imEvt;
+
+    if(imEvt.c == 24) //down
+        iy += 8;
+    else if(imEvt.c == 5) //up
+        iy -= 8;
+    else if(imEvt.c == 19) //left
+        ix -= 8;
+    else if(imEvt.c == 4) //right
+        ix += 8;
+    repaint();
 };
 
 xwin.setVisible(true);
