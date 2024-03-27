bb975385e5487646c89000c86bd7942aa7fb6b03
misazhu <misa.zhu@gmail.com>
2024-03-27 22:17:39 +0800

    xcores apps

--- system/full/apps/xcores/xcores.cc
+++ system/full/apps/xcores/xcores.cc
@@ -18,29 +18,38 @@
 using namespace Ewok;
 
 
-class GCores : public XWin {
+class XCores : public XWin {
 	static const uint32_t HEART_BIT_NUM = 32;
 	uint32_t index;
 	bool loop;
 	sys_info_t sysInfo;
 	uint32_t cores[MAX_CORE_NUM][HEART_BIT_NUM];
-	const  uint32_t colors[6] = {
-		0xff000000, //BLACK
-		0xffff0000, //RED
-		0xff00ff00, //GREEN
-		0xff0000ff, //BLUE
-		0xffff0088, //PURPLE
-		0xff00ffff //CYAN
+
+	int x_off;
+	int y_off_bottom;
+	int y_off;
+
+	static const uint32_t COLOR_NUM = 6;
+	const  uint32_t colors[COLOR_NUM] = {
+		0xff0000ff, 
+		0xffff0000, 
+		0xff8800ff,
+		0xff0088ff,
+		0xffff8800,
+		0xff000000
 	};
 public:
-	inline GCores() {
+	inline XCores() {
 		index = 0;
 		loop = false;
+		x_off = 10;
+		y_off = 20;
+		y_off_bottom = 10;
 		memset(cores, 0, sizeof(cores));
 		memset(&sysInfo, 0, sizeof(sys_info_t));
 	}
 	
-	inline ~GCores() {
+	inline ~XCores() {
 	}
 
 	void update_cores() {
@@ -60,7 +69,7 @@ public:
 
 protected:
 	void drawTitle(graph_t* g, uint32_t i, uint32_t color) {
-		int x = 10 + i*60;
+		int x = x_off + i*60;
 		graph_fill(g, x, 4, 10, 10, color);
 		char s[16];
 		snprintf(s, 15, "%d:%d%%", i, 100 - (sysInfo.core_idles[i]/10000));
@@ -74,32 +83,41 @@ protected:
 		for(uint32_t j=0; j<num; j++) {
 			uint32_t k = j;
 			if(loop)
-				k = j+index;
+				k = j + index;
 			if(k >= HEART_BIT_NUM)
 				k -= HEART_BIT_NUM;
 
-			int x = (j+1)*xstep;
-			int y = g->h - 10 - yzoom*(100 - (cores[i][k] / 10000)); //percentage
+			uint32_t perc = 100 - (cores[i][k] / 10000);
+
+			int x = (j+1) * xstep;
+			int y = g->h - y_off_bottom - yzoom*(perc); //percentage
 			if(last_y == 0)
 				last_y = y;
 
-			graph_wline(g, 10+last_x, last_y, 10+x, y, color, 3);
+			graph_wline(g, x_off+last_x, last_y, x_off+x, y, color, 3);
 			last_x = x;
 			last_y = y;
 		}
 	}
 
 	void drawBG(graph_t* g, float xstep, float yzoom) {
-		uint32_t color = 0xffdddddd;
+		uint32_t color = 0xffbbbbbb;
+		uint32_t bgColor = 0xff888888;
 		uint32_t w = xstep*HEART_BIT_NUM;
 		uint32_t h = yzoom*100;
 
+		graph_fill(g, x_off,
+				g->h - h - y_off_bottom,
+				w, h, bgColor);
+
 		for(uint32_t i=0; i<=HEART_BIT_NUM; i++) {
-			graph_line(g, 10+i*xstep, g->h-10, 10+ i*xstep, g->h-10-h, color);
+			graph_line(g, x_off + i*xstep, g->h - y_off_bottom,
+					x_off + i*xstep, g->h - y_off_bottom - h, color);
 		}
 
 		for(uint32_t i=0; i<=10; i++) {
-			graph_line(g, 10, g->h-10-i*10*yzoom, w+10, g->h-10-i*10*yzoom, color);
+			graph_line(g, x_off, g->h - y_off_bottom - i*10*yzoom,
+				w + x_off, g->h - y_off_bottom - i*10*yzoom, color);
 		}
 
 	}
@@ -110,14 +128,14 @@ protected:
 
 		graph_clear(g, 0xffffffff);
 
-		float xstep = (g->w - 10*2)/ (float)HEART_BIT_NUM;
-		float yzoom = (g->h - 20 - 10)/ 100.0;
+		float xstep = (g->w - x_off*2)/ (float)HEART_BIT_NUM;
+		float yzoom = (g->h - y_off - y_off_bottom)/ 100.0;
 		if(yzoom == 0)
 			yzoom = 1.0;
 
 		drawBG(g, xstep, yzoom);
 		for(uint32_t i=0; i<sysInfo.cores; i++) {
-			uint32_t color = colors[i%6];
+			uint32_t color = colors[i%COLOR_NUM];
 			drawTitle(g, i, color);
 			drawChat(g, i, xstep, yzoom, color);
 		}
@@ -125,7 +143,7 @@ protected:
 };
 
 
-static GCores* _xwin = NULL;
+static XCores* _xwin = NULL;
 static void timer_handler(void) {
 	_xwin->update_cores();
 }
@@ -135,8 +153,8 @@ int main(int argc, char* argv[]) {
 	(void)argv;
 
 	X x;
-	GCores xwin;
-	x.open(0, &xwin, -1, -1, 300, 210, "gcores", XWIN_STYLE_NORMAL);
+	XCores xwin;
+	x.open(0, &xwin, -1, -1, 300, 160, "xcores", XWIN_STYLE_NORMAL);
 	xwin.setVisible(true);
 
 	_xwin = &xwin;
