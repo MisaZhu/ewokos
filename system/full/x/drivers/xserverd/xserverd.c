fcc9100f1ea16eaa39cc0b2adb6cdbd5ff73b54f
misazhu <misa.zhu@gmail.com>
2024-04-04 03:09:15 +0800

    update

--- system/full/x/drivers/xserverd/xserverd.c
+++ system/full/x/drivers/xserverd/xserverd.c
@@ -1127,15 +1127,17 @@ static void mouse_xwin_handle(x_t* x, xwin_t* win, int pos, xevent_t* ev) {
 	}
 	else if(ev->state ==  XEVT_MOUSE_DRAG) {
 		if(win->xinfo->state != XWIN_STATE_MAX) {
-			x->current.win_drag = win;
-			if(pos == FRAME_R_TITLE) {//window title 
-				//x->current.win_drag = win;
+			if((win->xinfo->style & XWIN_STYLE_NO_FRAME) != 0) {
+				x->current.win_drag = win;
+			}
+			else if(pos == FRAME_R_TITLE) {//window title 
+				x->current.win_drag = win;
 				x->current.old_pos.x = x->cursor.cpos.x;
 				x->current.old_pos.y = x->cursor.cpos.y;
 				x->current.drag_state = X_win_DRAG_MOVE;
 			}
 			else if(pos == FRAME_R_RESIZE) {//window resize
-				//x->current.win_drag = win;
+				x->current.win_drag = win;
 				x->current.old_pos.x = x->cursor.cpos.x;
 				x->current.old_pos.y = x->cursor.cpos.y;
 				x->current.drag_state = X_win_DRAG_RESIZE;
