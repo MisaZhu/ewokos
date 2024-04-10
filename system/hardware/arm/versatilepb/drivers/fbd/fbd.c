02fa62b630fb86e695d995d8e90eb615239a0ac7
misazhu <misa.zhu@gmail.com>
2024-04-10 23:07:20 +0800

    update

--- system/hardware/arm/versatilepb/drivers/fbd/fbd.c
+++ system/hardware/arm/versatilepb/drivers/fbd/fbd.c
@@ -7,13 +7,32 @@
 #include <upng/upng.h>
 #include <arch/vpb/framebuffer.h>
 
+static graph_t* _g = NULL;
 static uint32_t flush(const fbinfo_t* fbinfo, const void* buf, uint32_t size, int rotate) {
-	(void)rotate;
-	uint32_t sz = 4 * fbinfo->width * fbinfo->height;
-	if(size < sz || fbinfo->depth != 32)
+	if(fbinfo->depth != 32)
 		return -1;
 
-	memcpy((void*)fbinfo->pointer, buf, sz);
+	uint32_t sz = 4 * fbinfo->width * fbinfo->height;
+	graph_t g;
+	if(rotate == G_ROTATE_N90 || rotate == G_ROTATE_90) {
+		graph_init(&g, buf, fbinfo->height, fbinfo->width);
+		if(_g == NULL)
+			_g = graph_new(fbinfo->pointer, fbinfo->width, fbinfo->height);
+	}
+	else if(rotate == G_ROTATE_180) {
+		graph_init(&g, buf, fbinfo->width, fbinfo->height);
+		if(_g == NULL)
+			_g = graph_new(fbinfo->pointer, fbinfo->width, fbinfo->height);
+	}
+
+	if(_g != NULL) {
+		graph_rotate_to(&g, _g, rotate);
+		if(fbinfo->pointer != _g->buffer)
+			memcpy((void*)fbinfo->pointer, _g->buffer, sz);
+	}
+	else  {
+		memcpy((void*)fbinfo->pointer, buf, sz);
+	}
 	return size;
 }
 
@@ -26,6 +45,7 @@ static int32_t init(uint32_t w, uint32_t h, uint32_t dep) {
 }
 
 int main(int argc, char** argv) {
+	_g = NULL;
 	fbd_t fbd;
 	const char* mnt_point = argc > 1 ? argv[1]: "/dev/fb0";
 
