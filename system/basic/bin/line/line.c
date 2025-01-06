83ad801e514bbe93d6d06527ff849e97615453a0
misazhu <misa.zhu@gmail.com>
2025-01-06 19:47:37 +0800

    update

--- system/basic/bin/line/line.c
+++ system/basic/bin/line/line.c
@@ -27,7 +27,7 @@ static char* get_line(char* ln, int32_t* sz, char c) {
 	}
 
 	*sz = len + BUF_SIZE;
-	ret = (char*)malloc(*sz+1);
+	ret = (char*)malloc(*sz);
 	strcpy(ret, ln);
 	ret[len] = c;
 	ret[len+1] = 0;
@@ -40,6 +40,8 @@ static char*   _line = NULL;
 static uint32_t _line_buf_size = 0;
 static uint32_t  _lines_rd  = 8;
 static uint32_t  _line_count  = 0;
+static uint32_t  _line_index  = 0;
+static bool _line_no  = false;
 	
 static void line(char* str, int32_t sz, const char* key) {
 	int32_t i = 0;
@@ -47,13 +49,21 @@ static void line(char* str, int32_t sz, const char* key) {
 		char c = str[i++];
 		if(c == 0 || c == '\r' || c == '\n') {
 			if(_line != NULL) {
-				printf("%s\n", _line);
+				if(_line_no)
+					printf("%06d: %s\n", _line_index, _line);
+				else
+					printf("%s\n", _line);
 
 				_line[0] = 0;
 				_line_count++;
+				_line_index++;
 
 				if(_line_count >= _lines_rd) {
-					break;
+					while(read(VFS_BACKUP_FD0, &c, 1) == 1) {
+						if(c == '\n' || c == '\r' || c == ' ')
+							break;
+					}
+					_line_count = 0;
 				}
 			}
 		}
@@ -62,14 +72,18 @@ static void line(char* str, int32_t sz, const char* key) {
 	}
 }
 
+
 static int doargs(int argc, char* argv[]) {
 	int c = 0;
 	while (c != -1) {
-		c = getopt (argc, argv, "n:");
+		c = getopt (argc, argv, "ln:");
 		if(c == -1)
 			break;
 
 		switch (c) {
+		case 'l':
+			_line_no = true;
+			break;
 		case 'n':
 			_lines_rd = atoi(optarg);
 			break;
@@ -88,6 +102,8 @@ int main(int argc, char* argv[]) {
 	_line_buf_size = 0;
 	_lines_rd  = 8;
 	_line_count  = 0;
+	_line_index  = 0;
+	_line_no  = false;
 
 	if(doargs(argc, argv) != 0)
 		return -1;
@@ -114,13 +130,9 @@ int main(int argc, char* argv[]) {
 		if(sz <= 0)
 			break;
 		line(buf, sz, argv[1]);
-
-		if(_line_count >= _lines_rd) {
-			break;
-		}
 	}
-	//if(_line[0] != 0)
-		//printf("%s\n", _line);
+	if(_line[0] != 0)
+		printf("%s\n", _line);
 	free(_line);
 	return 0;
 }
