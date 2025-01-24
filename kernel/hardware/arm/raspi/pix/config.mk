ce5a780debae6402f1645bec6177bc7dd8cf4dc9
misazhu <misa.zhu@gmail.com>
2025-01-25 05:07:55 +0800

    update

--- kernel/hardware/arm/raspi/pix/config.mk
+++ kernel/hardware/arm/raspi/pix/config.mk
@@ -4,9 +4,9 @@ ARCH_CFLAGS = -march=armv7ve -DSCHD_TRACE
 ARCH=v7
 
 #----PI4-------
-PI4=yes
+#PI4=yes
 #----PI4.CM4-------
-CM4=yes
+#CM4=yes
 #----enable DPI display---
 #DPI=yes
 #----multi core(SMP)------
