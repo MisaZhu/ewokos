848d1fc2994e6820f8bbd73162b3fb429b916997
misazhu <misa.zhu@gmail.com>
2025-01-31 01:12:02 +0800

    update

--- system/gui/bin/fbtest/fbtest.c
+++ system/gui/bin/fbtest/fbtest.c
@@ -4,6 +4,7 @@
 #include <font/font.h>
 #include <mouse/mouse.h>
 #include <keyb/keyb.h>
+#include <ewoksys/keydef.h>
 #include <fcntl.h>
 
 int main(int argc, char** argv) {
@@ -13,36 +14,45 @@ int main(int argc, char** argv) {
 		return -1;
 
 	graph_t *g = fb_fetch_graph(&fb);
-    graph_clear(g, 0xffffffff);
     
     font_t* font = font_new(DEFAULT_SYSTEM_FONT, true);
     graph_draw_text_font(g, 10, 10, "Hello!", font, 16, 0xff000000);
 
-    graph_fill_circle(g, 100, 100, 80, 0xffff0000);
-
-
     int mouse_fd = open("/dev/mouse0", O_RDONLY);
     int keyb_fd = open("/dev/keyb0", O_RDONLY);
-    while(true) {
+    int x = 0, y = 0;
+    bool dirty = true;
+    char txt[64] = {0};
+    bool quit = false;
+    while(!quit) {
+        if(dirty) {
+            graph_clear(g, 0xffffffff);
+            snprintf(txt, 63, "x:%d, y:%d", x, y);
+            graph_fill_round(g, x, y, 100, 20, 4, 0xffff0000);
+            graph_draw_text_font(g, x, y, txt, font, 12, 0xff000000);
+            dirty = false;
+            fb_flush(&fb, true);
+        }
+
         mouse_evt_t mevt;
         keyb_evt_t kevts[KEYB_EVT_MAX];
         if(mouse_read(mouse_fd, &mevt) == 1) {
-            klog("mouse: %d, %d, %d, %d\n",
-                mevt.state,
-                mevt.button,
-                mevt.rx,
-                mevt.ry);
+            x+= mevt.rx;
+            y+= mevt.ry;
+            if(x < 0) x = 0;
+            if(y < 0) y = 0;
+            if(x > g->w) x = g->w - 10;
+            if(y > g->h) y = g->h - 10;
+            dirty = true;
         }
 
         int n = keyb_read(keyb_fd, kevts, KEYB_EVT_MAX);
-        for(int i=0; i<n; i++)
-            klog("keyb: %d, %d\n", kevts[i].key, kevts[i].state);
-
+        for(int i=0; i<n; i++) {
+            if(kevts[i].key == KEY_ESC)
+                quit = true;
+        }
         usleep(3000);
     }
-
-    fb_flush(&fb, true);
-    sleep(1);
 
     font_free(font);
     close(mouse_fd);
