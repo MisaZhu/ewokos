06922c91759bab517495fdbefb82d471b259f4b5
misazhu <misa.zhu@gmail.com>
2025-02-07 21:59:31 +0800

    update

--- kernel/platform/arm/arch/common/include/mm/mmudef.h
+++ kernel/platform/arm/arch/common/include/mm/mmudef.h
@@ -16,7 +16,7 @@
 #define KERNEL_BASE                    0x80000000 //=2G virtual address start base.
 #define INTERRUPT_VECTOR_BASE          0xffff0000
 
-#define DMA_SIZE (1*MB)
+#define DMA_SIZE (512*KB)
 
 //#define MAX_MEM_SIZE                   (1*GB + 256*MB) //max usable memory for 32bits OS
 #define MAX_MEM_SIZE                   (1*GB) //max usable memory for 32bits OS
