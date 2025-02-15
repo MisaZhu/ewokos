f576bac5bf5d87d823c06093e6abfacf7f34ac2a
misazhu <misa.zhu@gmail.com>
2025-02-16 02:00:29 +0800

    update

--- kernel/platform/arm/raspi/lib/bcm283x/src/framebuffer.c
+++ kernel/platform/arm/raspi/lib/bcm283x/src/framebuffer.c
@@ -37,10 +37,10 @@ int32_t fb_init_raw(uint32_t w, uint32_t h, uint32_t dep) {
 
 	p_base = (0xC00000); //GPU addr to ARM addr
 	_fb_info.pointer = P2V(p_base);
-	_fb_info.size = w*h*(dep/8);
 	_fb_info.xoffset = 0;
 	_fb_info.yoffset = 0;
 	_fb_info.size_max = 32*MB;
+	_fb_info.size = _fb_info.size_max;
 #else
 	fb_init_t* fbinit = &_fbinit;
 	//fb_init_t* fbinit = (fb_init_t*)kalloc4k();
