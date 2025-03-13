39b0c420f2e96c668c7352548090e37656ac0964
misazhu <misa.zhu@gmail.com>
2025-03-14 04:20:12 +0800

    update

--- kernel/platform/arm/ev3/config.mk
+++ kernel/platform/arm/ev3/config.mk
@@ -1,5 +1,5 @@
 CPU = arm926ej-s
 QEMU_FLAGS = -cpu arm926 -M versatilepb -m 256M -serial mon:stdio 
-ARCH_CFLAGS = -fno-strict-aliasing -mcpu=$(CPU)  -DARM_V6 -DSCHD_TRACE -DKCONSOLE
+ARCH_CFLAGS = -fno-strict-aliasing -mcpu=$(CPU)  -DARM_V6 -DKCONSOLE
 ARCH=v5
 BSP=ev3
--- kernel/platform/arm/raspi/pix/config.mk
+++ kernel/platform/arm/raspi/pix/config.mk
@@ -1,6 +1,6 @@
 QEMU_FLAGS = -M raspi2b -m 1024M -serial mon:stdio
 
-ARCH_CFLAGS = -march=armv7ve -DSCHD_TRACE
+ARCH_CFLAGS = -march=armv7ve
 ARCH=v7
 
 #----PI4-------
