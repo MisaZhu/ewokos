2e3514bdd8b37d1f8e23ea826d31acc61ce1c453
misazhu <misa.zhu@gmail.com>
2025-03-23 14:53:46 +0800

    update

--- machine/raspix/kernel/pix/config.mk
+++ machine/raspix/kernel/pix/config.mk
@@ -23,8 +23,8 @@ ifeq ($(PI4),yes)
 #QEMU_CMD = qemu-system-aarch64
 QEMU_CMD = qemu-system-arm
 ARCH_CFLAGS += -DPI4
-QEMU_FLAGS = -M raspi4b -m 2048M -serial mon:stdio  -display cocoa
+QEMU_FLAGS = -M raspi4b -m 2048M -serial mon:stdio  -display sdl
 else
 QEMU_CMD = qemu-system-arm
-QEMU_FLAGS = -M raspi2b -m 1024M -serial mon:stdio -display cocoa
+QEMU_FLAGS = -M raspi2b -m 1024M -serial mon:stdio -display sdl
 endif
