af8a329def44223605bb51be8906c994c2cff1e7
misazhu <misa.zhu@gmail.com>
2025-04-17 02:45:30 +0800

    update

--- system/gui/libs/libvorbis/pkgconfig.mk
+++
@@ -1,7 +0,0 @@
-
-CFLAGS += -I../libvorbis/include -I../libogg/include
-
-LDFLAGS += -L../libvorbis -L../libogg
-
-LIBS := -lvorbis -logg $(LIBS)
-LIBS_DEP += ../libvorbis/libvorbis.a ../libogg/libogg.a
