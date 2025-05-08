a85476d2894c16fc44a8288baf59fa1dcdb965bc
misazhu <misa.zhu@gmail.com>
2025-05-08 22:46:44 +0800

    load elf code seg read only

--- kernel/kernel/src/proc.c
+++ kernel/kernel/src/proc.c
@@ -791,6 +791,7 @@ int32_t proc_load_elf(proc_t *proc, const char *image, uint32_t size) {
 					proc->space->heap_size,
 					V2P(page),
 					AP_RW_R, PTE_ATTR_WRBACK);
+				page = NULL;
 			}
 		}
 		prog_header_offset += sizeof(struct elf_program_header);
