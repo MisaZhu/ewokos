0b26006e8451aef7c74ef3c88515c6a1e1e5dc3a
misazhu <misa.zhu@gmail.com>
2025-05-26 22:25:09 +0800

    widgetx loop fps

--- system/xwin/libs/widget++/src/Widget/WidgetX.cc
+++ system/xwin/libs/widget++/src/Widget/WidgetX.cc
@@ -1,6 +1,7 @@
 #include <Widget/WidgetX.h>
 #include <x++/X.h>
 #include <ewoksys/proc.h>
+#include <ewoksys/kernel_tic.h>
 
 namespace Ewok {
 
@@ -11,11 +12,20 @@ static void loop(void* p) {
     if(win == NULL)
         return;
     RootWidget* root = win->getRoot();
+
+    uint64_t tik = kernel_tic_ms(0);
+	uint32_t tm = 10;// 100 fps(10ms)
+
     if(root != NULL) {
         win->doTimer();
         root->repaintWin();
     }
-    proc_usleep(10000);
+
+	uint32_t gap = (uint32_t)(kernel_tic_ms(0) - tik);
+	if(gap < tm) {
+		gap = tm - gap;
+		proc_usleep(gap*1000);
+	}
 }
 
 void widgetXRun(X* x, WidgetWin* win) {
