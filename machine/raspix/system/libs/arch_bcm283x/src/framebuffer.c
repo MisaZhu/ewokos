5c0c68d6e3ed9515c99dcef87d567e668195a1c4
misazhu <misa.zhu@gmail.com>
2025-06-28 02:58:32 +0800

    dma blocks

--- machine/raspix/system/libs/arch_bcm283x/src/framebuffer.c
+++ machine/raspix/system/libs/arch_bcm283x/src/framebuffer.c
@@ -29,7 +29,7 @@ int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
 	syscall1(SYS_GET_SYS_INFO, (ewokos_addr_t)&sysinfo);
 
 	bcm283x_mailbox_init();
-	//fb_init_t* fbinit = (fb_init_t*)dma_phy_addr(0, dma_alloc(sizeof(fb_init_t)));
+
 	fb_init_t* fbinit = (fb_init_t*)(dma_alloc(0, sizeof(fb_init_t)));
 	
 	mail_message_t msg;
@@ -42,7 +42,6 @@ int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
 	fbinit->vheight = fbinit->height;
 	fbinit->depth = dep;
 
-	//msg.data = (syscall1(SYS_V2P, (uint32_t)fbinit) | 0xC0000000) >> 4; // ARM addr to GPU addr
 	msg.data = (dma_phy_addr(0, (ewokos_addr_t)fbinit) | 0xC0000000) >> 4; // ARM addr to GPU addr
 	bcm283x_mailbox_send(FRAMEBUFFER_CHANNEL, &msg);
 	bcm283x_mailbox_read(FRAMEBUFFER_CHANNEL, &msg);
@@ -62,94 +61,13 @@ int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
 	_fb_info.size = fbinit->size;
 	_fb_info.xoffset = 0;
 	_fb_info.yoffset = 0;
-
-	/*if(_fb_info.pointer < sysinfo.kernel_base) {
-		_fb_info.pointer += sysinfo.kernel_base;
-	}
-	*/
 	_fb_info.size_max = sysinfo.gpu.max_size;
 	syscall3(SYS_MEM_MAP,(ewokos_addr_t) _fb_info.pointer, (ewokos_addr_t)_fb_info.phy_base, (ewokos_addr_t)_fb_info.size_max);
 	_fb_info.dma_id = dma_set(_fb_info.phy_base+_fb_info.size, _fb_info.size_max - _fb_info.size, true);
+
+	dma_free(0, (ewokos_addr_t)fbinit);
 	return 0;
 }
-
-/*
-int32_t bcm283x_fb_init(uint32_t w, uint32_t h, uint32_t dep) {
-	memset(&_fb_info, 0, sizeof(fbinfo_t));
-	bcm283x_mailbox_init();
-
-	uint32_t* mbox = (uint32_t*)dma_phy_addr(0, dma_alloc(0, 36));
-    mbox[0] = 35*4;
-    mbox[1] = 0; //request
-
-    mbox[2] = 0x48003;  //set phy wh
-    mbox[3] = 8;
-    mbox[4] = 8;
-    mbox[5] = w;
-    mbox[6] = h;
-
-    mbox[7] = 0x48004;  //set virt wh
-    mbox[8] = 8;
-    mbox[9] = 8;
-    mbox[10] = w;
-    mbox[11] = h;
-
-    mbox[12] = 0x48009; //set virt offset
-    mbox[13] = 8;
-    mbox[14] = 8;
-    mbox[15] = 0;       //FrameBufferInfo.x_offset
-    mbox[16] = 0;       //FrameBufferInfo.y.offset
-
-    mbox[17] = 0x48005; //set depth
-    mbox[18] = 4;
-    mbox[19] = 4;
-    mbox[20] = dep;      //FrameBufferInfo.depth
-
-    mbox[21] = 0x48006; //set pixel order
-    mbox[22] = 4;
-    mbox[23] = 4;
-    mbox[24] = 0;      //RGB, not BGR preferably
-
-    mbox[25] = 0x40001; //get framebuffer, gets alignment on request
-    mbox[26] = 8;
-    mbox[27] = 8;
-    mbox[28] = 4096;    //FrameBufferInfo.pointer
-    mbox[29] = 0;       //FrameBufferInfo.size
-
-    mbox[30] = 0x40008; //get pitch
-    mbox[31] = 4;
-    mbox[32] = 4;
-    mbox[33] = 0;       //FrameBufferInfo.pitch
-
-    mbox[34] = 0;
-
-	mail_message_t msg;
-	memset(&msg, 0, sizeof(mail_message_t));
-	msg.data = (syscall1(SYS_V2P, (ewokos_addr_t)&mbox | 0xC0000000)) >> 4; // ARM addr to GPU addr
-	bcm283x_mailbox_send(PROPERTY_CHANNEL, &msg);
-	bcm283x_mailbox_read(PROPERTY_CHANNEL, &msg);
-
-	klog("%x, %d\n", mbox[28], mbox[29]);
-    if(mbox[28] == 0 || mbox[29] == 0)
-		return -1;
-		
-	_fb_info.pointer = syscall1(SYS_P2V, mbox[28] & 0x3fffffff); //GPU addr to ARM addr
-	_fb_info.size = mbox[29];
-	_fb_info.width = mbox[5];
-	_fb_info.height = mbox[6];
-	_fb_info.pitch = mbox[33];
-	_fb_info.depth = mbox[20];
-	_fb_info.xoffset = 0;
-	_fb_info.yoffset = 0;
-
-	sys_info_t sysinfo;
-	syscall1(SYS_GET_SYS_INFO, (ewokos_addr_t)&sysinfo);
-
-	_fb_info.size_max = sysinfo.total_usable_mem_size - (_fb_info.pointer-sysinfo.kernel_base);
-	syscall3(SYS_MEM_MAP, (ewokos_addr_t)_fb_info.pointer, (ewokos_addr_t)_fb_info.pointer-sysinfo.kernel_base, (ewokos_addr_t)_fb_info.size_max);
-	return 0;
-}
-*/
 
 fbinfo_t* bcm283x_get_fbinfo(void) {
 	return &_fb_info;
