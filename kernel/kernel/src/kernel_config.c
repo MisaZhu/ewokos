b79d1c77648d68c11f1d2d50b0976d7f57af699c
misazhu <misa.zhu@gmail.com>
2025-08-10 01:24:03 +0800

    dma base buf fixed

--- kernel/kernel/src/kernel_config.c
+++ kernel/kernel/src/kernel_config.c
@@ -22,8 +22,13 @@ static void load_kernel_config_file() {
 		_kernel_config.cores = atoi(v);
 
 	v = sconf_get(sconf, "allocable_base_reserv_size");
-	if(v[0] != 0)
+	if(v[0] != 0) {
 		_allocable_phy_mem_base += ALIGN_UP(atoi(v), PAGE_SIZE);
+		_sys_info.sys_dma.phy_base = _allocable_phy_mem_base;
+		_sys_info.sys_dma.size = DMA_SIZE;
+		_sys_info.sys_dma.v_base = DMA_V_BASE;
+		_allocable_phy_mem_base += DMA_SIZE;
+	}
 	
 	v = sconf_get(sconf, "allocable_top_reserv_size");
 	if(v[0] != 0)
