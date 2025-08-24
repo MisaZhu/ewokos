24e6453917ba6a96a91b1a0fd14b4e1475bf741d
misazhu <misa.zhu@gmail.com>
2025-08-25 02:17:46 +0800

    wjs  javascript widget

--- system/xwin/data/test/layout.js
+++ system/xwin/data/test/layout.js
@@ -1,10 +1,24 @@
-function _onWidgetEvent(widgetID, widgetName, event) {
-    if(event.type != XEvent.MOUSE || event.state != XEvent.MOUSE_CLICK)
+function _onWidgetEvent(widget, event) {
+    if(event.type != XEvent.MOUSE || event.state == XEvent.MOUSE_MOVE)
         return;
-    //debug("widgetID: " + widgetID + ", widgetName: " + widgetName + ", event: " + event);
 
-    var widget = WJS.getWidgetByName(widgetName);
-    widget.set("label", "hello world!");
+    var image1 = WJS.getWidgetByName("image1");
+    var image2 = WJS.getWidgetByName("image2");
+
+    if(event.state == XEvent.MOUSE_DOWN)
+        widget.set("label", "down");
+    else if( event.state == XEvent.MOUSE_UP)
+        widget.set("label", "up");
+    else if( event.state == XEvent.MOUSE_CLICK) {
+        if(widget.get("name") == "button1") {
+            image1.set("file", "/usr/system/images/logos/apple.png");
+            image2.set("file", "/usr/system/images/logos/apple_old.png");
+        }
+        else if(widget.get("name") == "button2") {
+            image1.set("file", "/usr/system/images/logos/apple_old.png");
+            image2.set("file", "/usr/system/images/logos/apple.png");
+        }
+    }
 }
 
 function _onMenuItemEvent(menuID) {
