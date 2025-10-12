56933d414265fa6c3cb6523ee63af24e690c7e44
misazhu <misa.zhu@gmail.com>
2025-10-13 01:20:24 +0800

    klog dev

--- system/basic/libc/libewoksys/ewoksys/src/klog.c
+++ system/basic/libc/libewoksys/ewoksys/src/klog.c
@@ -14,7 +14,8 @@ extern "C" {
 
 #define BUF_SIZE 256
 static char _buf[BUF_SIZE+1];
-static int32_t _log_fd = -1;
+static int32_t _slog_fd = -1;
+static int32_t _klog_fd = -1;
 
 void kout(const char *str) {
 	syscall2(SYS_KPRINT, (ewokos_addr_t)str, (ewokos_addr_t)strlen(str));
@@ -25,7 +26,18 @@ void klog(const char *format, ...) {
 	va_start(ap, format);
 	vsnprintf(_buf, sizeof(_buf), format, ap);
 	va_end(ap);
-	syscall2(SYS_KPRINT, (ewokos_addr_t)_buf, (ewokos_addr_t)strlen(_buf));
+
+	const char* log_dev = "/dev/klog";
+	if(_klog_fd <= 0 && log_dev != NULL) {
+		_klog_fd = open(log_dev, O_WRONLY);
+	}
+
+	if(_klog_fd > 0) {
+		write(_klog_fd, _buf, strlen(_buf));
+	}
+	else {
+		syscall2(SYS_KPRINT, (ewokos_addr_t)_buf, (ewokos_addr_t)strlen(_buf));
+	}
 }
 
 void slog(const char *format, ...) {
@@ -35,12 +47,12 @@ void slog(const char *format, ...) {
 	va_end(ap);
 
 	const char* log_dev = "/dev/log";
-	if(_log_fd <= 0 && log_dev != NULL) {
-		_log_fd = open(log_dev, O_WRONLY);
+	if(_slog_fd <= 0 && log_dev != NULL) {
+		_slog_fd = open(log_dev, O_WRONLY);
 	}
 
-	if(_log_fd > 0) {
-		write(_log_fd, _buf, strlen(_buf));
+	if(_slog_fd > 0) {
+		write(_slog_fd, _buf, strlen(_buf));
 	}
 	else {
 		syscall2(SYS_KPRINT, (ewokos_addr_t)_buf, (ewokos_addr_t)strlen(_buf));
