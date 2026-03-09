b6e38d46910d3f8014f7f9f96d09c448e18f2b98
misazhu <misa.zhu@gmail.com>
2026-03-09 22:51:29 +0800

    ipconfig cmd

---
+++ system/basic/bin/ipconfig/ipconfig.c
@@ -0,0 +1,107 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ewoksys/vdevice.h>
+#include <ewoksys/keydef.h>
+#include <ewoksys/mstr.h>
+#include <ewoksys/proc.h>
+
+void putch(int c);
+
+int32_t cmd_gets(int fd, str_t* buf) {
+	str_reset(buf);	
+
+	while(1) {
+		char c, old_c;
+		int i = read(fd, &c, 1);
+		if(i <= 0 || c == 0) {
+		 	if(i == 0)
+			 	return -1;
+			proc_usleep(10000);
+			continue;
+		}
+
+		if (c == KEY_BACKSPACE) {
+			if (buf->len > 0) {
+				//delete last char
+				putch(CONSOLE_LEFT); 
+				putch(' ');
+				putch(CONSOLE_LEFT); 
+				buf->len--;
+			}
+		}
+		else if (c == CONSOLE_LEFT) {
+			if (buf->len > 0) {
+				//delete last char
+				putch(c); 
+				buf->len--;
+			}
+		}
+		else {
+			if(c == '\r') {
+				old_c = c;
+				c = '\n';
+			}
+			else  {
+				old_c = 0;
+				if(c == '\n' && old_c == '\r')
+					continue;
+			}
+
+			putch(c);
+			if(c == '\n')
+				break;
+			if(c > 27)
+				str_addc(buf, c);
+		}
+	}
+	str_addc(buf, 0);
+	return 0;
+}
+
+static void prompt(const char* dev_name) {
+	printf("devcmd: %s> ", dev_name);
+}
+
+int devcmd(const char* dev_name) {
+	str_t* cmd = str_new("");
+	while(true) {
+		prompt(dev_name);
+		cmd_gets(0, cmd);
+		if(cmd->len == 0 || strcmp(cmd->cstr, "\n") == 0)
+			continue;
+		if(strcmp(cmd->cstr, "exit") == 0)
+			break;
+
+		char* ret = dev_cmd(dev_name, cmd->cstr);
+		if(ret == NULL) {
+			printf("command excute failed!\n");
+		}
+		else {
+			printf("%s", ret);
+			free(ret);
+		}
+	}
+
+	str_free(cmd);
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	setbuf(stdout, NULL);
+	const char* dev_name = "/dev/net0";
+	if(argc > 1) {
+		dev_name = argv[1];
+	}
+
+	char* ret = dev_cmd(dev_name, "ip");
+	if(ret == NULL) {
+		printf("get network config failed!\n");
+		return -1;
+	}
+
+	printf("%s", ret);
+	free(ret);
+	return 0;
+}
