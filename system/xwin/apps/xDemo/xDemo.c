9d58f8bf9d3977d9c78de3a489553c62f25746e7
misazhu <misa.zhu@gmail.com>
2026-03-25 03:32:09 +0800

    round_3d

--- system/xwin/apps/xDemo/xDemo.c
+++ system/xwin/apps/xDemo/xDemo.c
@@ -128,7 +128,7 @@ static void on_repaint(xwin_t* xwin, graph_t* g) {
 	}
 	else if(_xtest_info.mode == ROUND) {
 		graph_fill_round(g, x, y, w, h, 12, c);
-		graph_round(g, x-4, y-4, w+8, h+8, 16, c);
+		graph_round(g, x-4, y-4, w+8, h+8, 16, 2, c);
 	}
 	else if(_xtest_info.mode == RECT) {
 		graph_fill(g, x, y, w, h, c);
