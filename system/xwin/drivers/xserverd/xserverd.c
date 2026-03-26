a7fa3c7473d509f833ac6084db6af31eb143214d
misazhu <misa.zhu@gmail.com>
2026-03-26 17:20:33 +0800

    SpriteWin

--- system/xwin/drivers/xserverd/xserverd.c
+++ system/xwin/drivers/xserverd/xserverd.c
@@ -1448,11 +1448,13 @@ static void mouse_xwin_handle(x_t* x, xwin_t* win, int pos, xevent_t* ev) {
 		}
 		
 		if(pos == FRAME_R_TITLE) {//window title 
+			x->current.win_drag = win;
 			x->current.old_pos.x = x->cursor.cpos.x;
 			x->current.old_pos.y = x->cursor.cpos.y;
 			x->current.drag_state = X_win_DRAG_MOVE;
 		}
 		else if(pos == FRAME_R_RESIZE) {//window resize
+			x->current.win_drag = win;
 			x->current.old_pos.x = x->cursor.cpos.x;
 			x->current.old_pos.y = x->cursor.cpos.y;
 			x->current.drag_state = X_win_DRAG_RESIZE;
