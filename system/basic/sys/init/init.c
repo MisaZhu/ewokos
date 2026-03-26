1357a253adfaeb1ecee0375049c3907bcdda2701
misazhu <misa.zhu@gmail.com>
2026-03-26 22:55:25 +0800

    sdc buffer release

--- system/basic/sys/init/init.c
+++ system/basic/sys/init/init.c
@@ -18,7 +18,7 @@
 
 static void* sd_read_ext2(const char* fname, int32_t* size) {
 	ext2_t ext2;
-	ext2_init(&ext2, sd_read, NULL, 0);
+	ext2_init(&ext2, sd_read, NULL, 1024*1024);
 	void* ret = ext2_readfile(&ext2, fname, size);
 	ext2_quit(&ext2);
 	return ret;
