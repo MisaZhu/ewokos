092eed1d975ccfe40def35a50341a12096f4add0
misazhu <misa.zhu@gmail.com>
2026-04-22 03:49:30 +0800

    netd tcp block bug fixed

--- system/network/drivers/netd/stack/udp.c
+++ system/network/drivers/netd/stack/udp.c
@@ -240,8 +240,6 @@ udp_output(struct ip_endpoint *src, struct ip_endpoint *dst, const  uint8_t *dat
     pseudo.len = hton16(total);
     psum = ~cksum16((uint16_t *)&pseudo, sizeof(pseudo), 0);
     hdr->sum = cksum16((uint16_t *)hdr, total, psum);
-    errorf("%s => %s, len=%u (payload=%zu)",
-        ip_endpoint_ntop(src, ep1, sizeof(ep1)), ip_endpoint_ntop(dst, ep2, sizeof(ep2)), total, len);
     udp_dump((uint8_t *)hdr, total);
     int ret = ip_output(IP_PROTOCOL_UDP, (uint8_t *)hdr, total, src->addr, dst->addr);
     TRACE();
