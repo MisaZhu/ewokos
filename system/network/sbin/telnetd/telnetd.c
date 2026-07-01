1a27481fed4959a16a61c98b497d144e8a4ebe83
misazhu <misa.zhu@gmail.com>
2026-07-02 06:53:31 +0800

    Fix stale pipe poll events causing sshd busy-spin

--- system/network/sbin/telnetd/telnetd.c
+++ system/network/sbin/telnetd/telnetd.c
@@ -5,12 +5,14 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <arpa/inet.h>
+#include <poll.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <ewoksys/core.h>
 #include <ewoksys/klog.h>
 #include <ewoksys/proc.h>
 #include <ewoksys/vfs.h>
+#include <ewoksys/wait.h>
 #include <setenv.h>
 
 #define EXIT_FAILURE -1
@@ -18,6 +20,8 @@
 
 #define BUF_SIZE 1024
 #define SMALL_BUF_SIZE 100
+#define PIPE_READ 0
+#define PIPE_WRITE 1
 
 #define SERVER_PORT 23
 
@@ -46,6 +50,100 @@ static int telnet_write_all(int fd, const uint8_t *buf, size_t len) {
         return -1;
     }
     return 0;
+}
+
+typedef struct {
+    int prev_cr;
+} telnet_input_state_t;
+
+typedef struct {
+    int prev_cr;
+} telnet_output_state_t;
+
+static void close_fd_if_valid(int *fd) {
+    if(fd != NULL && *fd >= 0) {
+        close(*fd);
+        *fd = -1;
+    }
+}
+
+static ssize_t normalize_telnet_input(
+        telnet_input_state_t *state, uint8_t *buf, ssize_t len) {
+    ssize_t out = 0;
+
+    for(ssize_t i = 0; i < len; ++i) {
+        uint8_t c = buf[i];
+
+        if(state->prev_cr) {
+            state->prev_cr = 0;
+            if(c == '\n' || c == '\0') {
+                buf[out++] = '\n';
+                continue;
+            }
+            buf[out++] = '\n';
+        }
+
+        if(c == '\r') {
+            state->prev_cr = 1;
+            continue;
+        }
+        buf[out++] = c;
+    }
+
+    return out;
+}
+
+static ssize_t flush_telnet_input_tail(telnet_input_state_t *state, uint8_t *buf) {
+    if(!state->prev_cr) {
+        return 0;
+    }
+    state->prev_cr = 0;
+    buf[0] = '\n';
+    return 1;
+}
+
+static ssize_t expand_telnet_output(
+        telnet_output_state_t *state,
+        const uint8_t *input,
+        ssize_t len,
+        uint8_t *output) {
+    ssize_t out = 0;
+
+    for(ssize_t i = 0; i < len; ++i) {
+        uint8_t c = input[i];
+
+        if(state->prev_cr) {
+            state->prev_cr = 0;
+            if(c == '\n') {
+                output[out++] = '\r';
+                output[out++] = '\n';
+                continue;
+            }
+            output[out++] = '\r';
+        }
+
+        if(c == '\r') {
+            state->prev_cr = 1;
+            continue;
+        }
+        if(c == '\n') {
+            output[out++] = '\r';
+            output[out++] = '\n';
+            continue;
+        }
+        output[out++] = c;
+    }
+
+    return out;
+}
+
+static ssize_t flush_telnet_output_tail(telnet_output_state_t *state, uint8_t *buf) {
+    if(!state->prev_cr) {
+        return 0;
+    }
+    state->prev_cr = 0;
+    buf[0] = '\r';
+    return 1;
 }
 
 static void telnet_send_initial_negotiation(int fd) {
@@ -94,6 +192,153 @@ static int telnet_open_server_socket(int port) {
     }
 }
 
+static void run_session_child(
+        int serv_sock,
+        int clnt_sock,
+        int child_stdin[2],
+        int child_stdout[2]) {
+    close_fd_if_valid(&serv_sock);
+    close_fd_if_valid(&clnt_sock);
+    close_fd_if_valid(&child_stdin[PIPE_WRITE]);
+    close_fd_if_valid(&child_stdout[PIPE_READ]);
+
+    int r0 = dup2(child_stdin[PIPE_READ], 0);
+    int r1 = dup2(child_stdout[PIPE_WRITE], 1);
+    int r2 = dup2(child_stdout[PIPE_WRITE], 2);
+    int rb0 = dup2(child_stdin[PIPE_READ], VFS_BACKUP_FD0);
+    int rb1 = dup2(child_stdout[PIPE_WRITE], VFS_BACKUP_FD1);
+    if(r0 < 0 || r1 < 0 || r2 < 0 || rb0 < 0 || rb1 < 0) {
+        exit(-1);
+    }
+
+    close_fd_if_valid(&child_stdin[PIPE_READ]);
+    close_fd_if_valid(&child_stdout[PIPE_WRITE]);
+    setenv("CONSOLE_ID", "telnet");
+    if(proc_exec("/bin/session") < 0) {
+        exit(-1);
+    }
+    exit(0);
+}
+
+static int relay_telnet_session(int clnt_sock, int child_stdin_w, int child_stdout_r) {
+    struct pollfd fds[2];
+    telnet_input_state_t input_state = {0};
+    telnet_output_state_t output_state = {0};
+    uint8_t input_buf[BUF_SIZE];
+    uint8_t output_buf[BUF_SIZE];
+    uint8_t wire_buf[BUF_SIZE * 2 + 2];
+
+    while(true) {
+        memset(fds, 0, sizeof(fds));
+        fds[0].fd = clnt_sock;
+        fds[0].events = POLLIN;
+        fds[1].fd = child_stdout_r;
+        fds[1].events = POLLIN;
+
+        int pret = poll(fds, 2, -1);
+        if(pret < 0) {
+            if(errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+
+        if((fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) != 0) {
+            ssize_t flush_len = flush_telnet_input_tail(&input_state, input_buf);
+            if(flush_len > 0 && telnet_write_all(child_stdin_w, input_buf, (size_t)flush_len) < 0) {
+                return -1;
+            }
+            return 0;
+        }
+
+        if((fds[1].revents & (POLLERR | POLLHUP | POLLNVAL)) != 0) {
+            ssize_t flush_len = flush_telnet_output_tail(&output_state, wire_buf);
+            if(flush_len > 0 && telnet_write_all(clnt_sock, wire_buf, (size_t)flush_len) < 0) {
+                return -1;
+            }
+            return 0;
+        }
+
+        if((fds[0].revents & POLLIN) != 0) {
+            ssize_t rd = read(clnt_sock, input_buf, sizeof(input_buf));
+            if(rd < 0) {
+                if(errno != EINTR) {
+                    return -1;
+                }
+            } else if(rd == 0) {
+                ssize_t flush_len = flush_telnet_input_tail(&input_state, input_buf);
+                if(flush_len > 0 && telnet_write_all(child_stdin_w, input_buf, (size_t)flush_len) < 0) {
+                    return -1;
+                }
+                return 0;
+            } else {
+                rd = normalize_telnet_input(&input_state, input_buf, rd);
+                if(rd > 0 && telnet_write_all(child_stdin_w, input_buf, (size_t)rd) < 0) {
+                    return -1;
+                }
+            }
+        }
+
+        if((fds[1].revents & POLLIN) != 0) {
+            ssize_t rd = read(child_stdout_r, output_buf, sizeof(output_buf));
+            if(rd < 0) {
+                if(errno != EINTR) {
+                    return -1;
+                }
+            } else if(rd == 0) {
+                ssize_t flush_len = flush_telnet_output_tail(&output_state, wire_buf);
+                if(flush_len > 0 && telnet_write_all(clnt_sock, wire_buf, (size_t)flush_len) < 0) {
+                    return -1;
+                }
+                return 0;
+            } else {
+                ssize_t wr = expand_telnet_output(&output_state, output_buf, rd, wire_buf);
+                if(wr > 0 && telnet_write_all(clnt_sock, wire_buf, (size_t)wr) < 0) {
+                    return -1;
+                }
+            }
+        }
+    }
+}
+
+static void run_telnet_worker(int serv_sock, int clnt_sock) {
+    int child_stdin[2] = {-1, -1};
+    int child_stdout[2] = {-1, -1};
+
+    close_fd_if_valid(&serv_sock);
+
+    if(pipe(child_stdin) != 0 || pipe(child_stdout) != 0) {
+        close_fd_if_valid(&clnt_sock);
+        exit(-1);
+    }
+
+    int pid = fork();
+    if(pid < 0) {
+        close_fd_if_valid(&child_stdin[PIPE_READ]);
+        close_fd_if_valid(&child_stdin[PIPE_WRITE]);
+        close_fd_if_valid(&child_stdout[PIPE_READ]);
+        close_fd_if_valid(&child_stdout[PIPE_WRITE]);
+        close_fd_if_valid(&clnt_sock);
+        exit(-1);
+    }
+
+    if(pid == 0) {
+        run_session_child(serv_sock, clnt_sock, child_stdin, child_stdout);
+    }
+
+    close_fd_if_valid(&child_stdin[PIPE_READ]);
+    close_fd_if_valid(&child_stdout[PIPE_WRITE]);
+
+    telnet_send_initial_negotiation(clnt_sock);
+    (void)relay_telnet_session(clnt_sock, child_stdin[PIPE_WRITE], child_stdout[PIPE_READ]);
+
+    close_fd_if_valid(&child_stdin[PIPE_WRITE]);
+    close_fd_if_valid(&child_stdout[PIPE_READ]);
+    close_fd_if_valid(&clnt_sock);
+    waitpid(pid);
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
@@ -107,55 +352,15 @@ int main(int argc, char *argv[])
     while (true) {
         clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &clnt_addr_size);
         if(clnt_sock > 0){
-            int sync_pipe[2] = {-1, -1};
-            if(pipe(sync_pipe) != 0) {
-                close(clnt_sock);
-                continue;
-            }
-
             int pid = fork();
             if(pid < 0) {
-                close(sync_pipe[0]);
-                close(sync_pipe[1]);
                 close(clnt_sock);
                 continue;
             }
-            if(pid == 0) {//child
-                char ok = 0;
-                close(sync_pipe[0]);
-                int r0 = dup2(clnt_sock, 0);
-                int r1 = dup2(clnt_sock, 1);
-                int r2 = dup2(clnt_sock, 2);
-                int rb0 = dup2(clnt_sock, VFS_BACKUP_FD0);
-                int rb1 = dup2(clnt_sock, VFS_BACKUP_FD1);
-                (void)r0;
-                (void)r1;
-                (void)r2;
-                (void)rb0;
-                (void)rb1;
-                if(r0 < 0 || r1 < 0 || r2 < 0 || rb0 < 0 || rb1 < 0) {
-                    write(sync_pipe[1], &ok, 1);
-                    close(sync_pipe[1]);
-                    close(clnt_sock);
-                    close(serv_sock);
-                    exit(-1);
-                }
-                ok = 1;
-                write(sync_pipe[1], &ok, 1);
-                close(sync_pipe[1]);
-                close(serv_sock);
-                telnet_send_initial_negotiation(STDOUT_FILENO);
-                close(clnt_sock);
-		        setenv("CONSOLE_ID", "telnet");
-                if(proc_exec("/bin/session") < 0) {
-                    exit(-1);
-                }
+            if(pid == 0) {
+                run_telnet_worker(serv_sock, clnt_sock);
             }
             else {
-                char ok = 0;
-                close(sync_pipe[1]);
-                read(sync_pipe[0], &ok, 1);
-                close(sync_pipe[0]);
                 close(clnt_sock);
             }
         }
