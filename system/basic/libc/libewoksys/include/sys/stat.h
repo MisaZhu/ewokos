43d571e0787d90331b947ef5659fd02cc08fe9e2
misazhu <misa.zhu@gmail.com>
2026-07-12 00:13:36 +0800

    posix standard

--- system/basic/libc/libewoksys/include/sys/stat.h
+++ system/basic/libc/libewoksys/include/sys/stat.h
@@ -19,6 +19,100 @@ struct stat {
 	time_t        st_ctime;   /* time of last status change - */
 };
 
+#ifndef S_IFMT
+#define S_IFMT   0170000
+#endif
+#ifndef S_IFSOCK
+#define S_IFSOCK 0140000
+#endif
+#ifndef S_IFLNK
+#define S_IFLNK  0120000
+#endif
+#ifndef S_IFREG
+#define S_IFREG  0100000
+#endif
+#ifndef S_IFBLK
+#define S_IFBLK  0060000
+#endif
+#ifndef S_IFDIR
+#define S_IFDIR  0040000
+#endif
+#ifndef S_IFCHR
+#define S_IFCHR  0020000
+#endif
+#ifndef S_IFIFO
+#define S_IFIFO  0010000
+#endif
+
+#ifndef S_ISUID
+#define S_ISUID  0004000
+#endif
+#ifndef S_ISGID
+#define S_ISGID  0002000
+#endif
+#ifndef S_ISVTX
+#define S_ISVTX  0001000
+#endif
+#ifndef S_IRUSR
+#define S_IRUSR  0000400
+#endif
+#ifndef S_IWUSR
+#define S_IWUSR  0000200
+#endif
+#ifndef S_IXUSR
+#define S_IXUSR  0000100
+#endif
+#ifndef S_IRGRP
+#define S_IRGRP  0000040
+#endif
+#ifndef S_IWGRP
+#define S_IWGRP  0000020
+#endif
+#ifndef S_IXGRP
+#define S_IXGRP  0000010
+#endif
+#ifndef S_IROTH
+#define S_IROTH  0000004
+#endif
+#ifndef S_IWOTH
+#define S_IWOTH  0000002
+#endif
+#ifndef S_IXOTH
+#define S_IXOTH  0000001
+#endif
+
+#ifndef S_IRWXU
+#define S_IRWXU  (S_IRUSR | S_IWUSR | S_IXUSR)
+#endif
+#ifndef S_IRWXG
+#define S_IRWXG  (S_IRGRP | S_IWGRP | S_IXGRP)
+#endif
+#ifndef S_IRWXO
+#define S_IRWXO  (S_IROTH | S_IWOTH | S_IXOTH)
+#endif
+
+#ifndef S_ISREG
+#define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)
+#endif
+#ifndef S_ISDIR
+#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)
+#endif
+#ifndef S_ISCHR
+#define S_ISCHR(m)  (((m) & S_IFMT) == S_IFCHR)
+#endif
+#ifndef S_ISBLK
+#define S_ISBLK(m)  (((m) & S_IFMT) == S_IFBLK)
+#endif
+#ifndef S_ISFIFO
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
+#endif
+#ifndef S_ISLNK
+#define S_ISLNK(m)  (((m) & S_IFMT) == S_IFLNK)
+#endif
+#ifndef S_ISSOCK
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)
+#endif
+
 #define F_OK    0
 #define R_OK    4
 #define W_OK    2
